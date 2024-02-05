0f17ba5a5d1e3b601607685e5860280c6a3000bf
Alexander Hanke <alexander.hanke@ntnu.no>
2024-02-05 15:06:12 +0100

    Renamed class data

--- src/data_f.h
+++ src/data_f.h
@@ -33,7 +33,7 @@ using namespace std;
 #ifndef DATA_F_H_
 #define DATA_F_H_
 
-class data_f : public data, public increment
+class data_f : public data_b, public increment
 {
 public:
 	data_f(lexer*, fdm*, ghostcell*);
