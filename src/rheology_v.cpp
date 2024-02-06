4383f7a474daaa3e0aa82e1181cfcfd4371678d4
Alexander Hanke <alexander.hanke@ntnu.no>
2024-02-06 14:31:59 +0100

    Fixed typo

--- src/rheology_v.cpp
+++ src/rheology_v.cpp
@@ -19,10 +19,11 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------
 Author: Hans Bihs
 --------------------------------------------------------------------*/
-#include"rheology_v.h"
+
+#include"rheology_v.h"
 #include"lexer.h"
 #include"fdm.h"
-#include"ghostcell.h" a
+#include"ghostcell.h"
 #include"diff_void.h"
 #include"ediff2.h"
 #include"idiff2.h"
