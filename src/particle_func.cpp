f10254c49cbc36b32dc7fec5e858846de3273aa4
Alexander Hanke <alexander.hanke@ntnu.no>
2024-02-20 16:36:26 +0100

    Updated particle_func

--- src/particle_func.cpp
+++ src/particle_func.cpp
@@ -141,10 +141,10 @@ void particle_func::advect(lexer* p, fdm* a, particles_obj* PP, int minflag, dou
 /// @param p 
 /// @param a 
 /// @param PP 
-/// @param minflag Particles need to have a larger flag
+/// @param flag Particles need to have a larger flag than this
 /// Uses Runge Kutta 3 to calculate the change in particle velocity 
 /// The particle velocity is then used to transport the particle
-void particle_func::transport(lexer* p, fdm* a, particles_obj* PP, int minflag)
+void particle_func::transport(lexer* p, fdm* a, particles_obj* PP, int flag)
 {
     double RKu,RKv,RKw;
     double u,v,w;
@@ -155,7 +155,7 @@ void particle_func::transport(lexer* p, fdm* a, particles_obj* PP, int minflag)
     double pressureDiv=0, stressDiv=0;
 
     PARTICLELOOP
-        if(PP->Flag[n]>minflag)
+        if(PP->Flag[n]>flag)
         {
             // Prep
             i=p->posc_i(PP->X[n]);
@@ -701,7 +701,7 @@ void particle_func::make_moving(lexer* p, fdm* a, particles_obj* PP)
         j=p->posc_j(PP->Y[n]);
         k=p->posc_k(PP->Z[n]);
 
-        thetas=(PI*pow(PP->d50,3.0)*PP->cellSum[IJK]/(6.0*p->DXN[IP]*p->DYN[JP]*p->DYN[KP]));
+        thetas=0;//(PI*pow(PP->d50,3.0)*PP->cellSum[IJK]/(6.0*p->DXN[IP]*p->DYN[JP]*p->DYN[KP]));
         u=p->ccipol1(a->u,PP->X[n],PP->Y[n],PP->Z[n]);
         v=p->ccipol1(a->v,PP->X[n],PP->Y[n],PP->Z[n]);
         w=p->ccipol1(a->w,PP->X[n],PP->Y[n],PP->Z[n]);
