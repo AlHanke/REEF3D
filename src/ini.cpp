8da648b725125e6ea6b66426d4b09368ac039656
Alexander Hanke <alexander.hanke@ntnu.no>
2024-02-21 11:41:01 +0100

    Added description to Q61

--- src/ini.cpp
+++ src/ini.cpp
@@ -766,7 +766,7 @@ void lexer::ini_default()
     Q31=0.001;          // double particle diameter
     Q41=0.5;            // double porosity
     Q43=0;              // int number of water iteration, before particle transport starts
-    Q61=0;
+    Q61=0;              // point source x,y,z, iterations between particles
     Q101=0;             // int ini particle as topo
     Q102=0.9;           // double max distance from topo = Q102*dz
     Q110=0;             // int ini particle as box
