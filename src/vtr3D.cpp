a1a0e76a92995fb6461b51206683b70bdfb7c561
Alexander Hanke <alexander.hanke@ntnu.no>
2024-12-04 14:10:32 +0100

    Replaced tabs with spaces in printer_CFD and vtk files

--- src/vtr3D.cpp
+++ src/vtr3D.cpp
@@ -36,42 +36,42 @@ vtr3D::~vtr3D()
 
 void vtr3D::folder(const char* A10)
 {
-	sprintf(pname,"./REEF3D_%s_VTR", A10);
-	mkdir(pname,0777);
+    sprintf(pname,"./REEF3D_%s_VTR", A10);
+    mkdir(pname,0777);
 }
 
 void vtr3D::offset(lexer *p, int *offset, int &n)
 {
-	//x
+    //x
     offset[n]=offset[n-1]+4+4*(p->knox+1); 
     ++n;
-	//y
-	offset[n]=offset[n-1]+4+4*(p->knoy+1); 
+    //y
+    offset[n]=offset[n-1]+4+4*(p->knoy+1); 
     ++n;
-	//z
-	offset[n]=offset[n-1]+4+4*(p->knoz+1); 
+    //z
+    offset[n]=offset[n-1]+4+4*(p->knoz+1); 
     ++n;
 }
 
 void vtr3D::beginning(lexer *p, std::ofstream &result)
 {
-	xmlVersion(result);
-	result<<"<VTKFile type=\"RectilinearGrid\" ";
-	vtkVersion(result);
-	result<<"<RectilinearGrid WholeExtent=\""<<p->origin_i<<" "<<p->origin_i+p->knox<<" "<<p->origin_j<<" "<<p->origin_j+p->knoy<<" "<<p->origin_k<<" "<<p->origin_k+p->knoz<<" "<<"\">\n";
+    xmlVersion(result);
+    result<<"<VTKFile type=\"RectilinearGrid\" ";
+    vtkVersion(result);
+    result<<"<RectilinearGrid WholeExtent=\""<<p->origin_i<<" "<<p->origin_i+p->knox<<" "<<p->origin_j<<" "<<p->origin_j+p->knoy<<" "<<p->origin_k<<" "<<p->origin_k+p->knoz<<" "<<"\">\n";
     if(p->P16==1)
     {
         timeValue(result,p->simtime);
     }
-	result<<"<Piece Extent=\""<<p->origin_i<<" "<<p->origin_i+p->knox<<" "<<p->origin_j<<" "<<p->origin_j+p->knoy<<" "<<p->origin_k<<" "<<p->origin_k+p->knoz<<"\">\n";
+    result<<"<Piece Extent=\""<<p->origin_i<<" "<<p->origin_i+p->knox<<" "<<p->origin_j<<" "<<p->origin_j+p->knoy<<" "<<p->origin_k<<" "<<p->origin_k+p->knoz<<"\">\n";
 }
 
 void vtr3D::beginningParallel(lexer *p, std::ofstream &result)
 {
-	xmlVersion(result);
-	result<<"<VTKFile type=\"PRectilinearGrid\" ";
-	vtkVersion(result);
-	result<<"<PRectilinearGrid WholeExtent=\"0 "<<p->gknox<<" 0 "<<p->gknoy<<" 0 "<<p->gknoz<<"\" GhostLevel=\"0\" Origin=\"0 0 0\" Spacing=\"1 1 1\">\n";
+    xmlVersion(result);
+    result<<"<VTKFile type=\"PRectilinearGrid\" ";
+    vtkVersion(result);
+    result<<"<PRectilinearGrid WholeExtent=\"0 "<<p->gknox<<" 0 "<<p->gknoy<<" 0 "<<p->gknoz<<"\" GhostLevel=\"0\" Origin=\"0 0 0\" Spacing=\"1 1 1\">\n";
     if(p->P16==1)
     {
         timeValue(result,p->simtime);
@@ -80,46 +80,46 @@ void vtr3D::beginningParallel(lexer *p, std::ofstream &result)
 
 void vtr3D::ending(std::ofstream &result, const int *offset, int &n)
 {
-	result<<"<Coordinates>\n";
-	result<<"\t<DataArray type=\"Float32\" Name=\"X\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
-	n++;
-	result<<"\t<DataArray type=\"Float32\" Name=\"Y\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
-	n++;
-	result<<"\t<DataArray type=\"Float32\" Name=\"Z\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
-	n++;
-	result<<"</Coordinates>\n";
-	result<<"</Piece>\n";
-	result<<"</RectilinearGrid>\n";
+    result<<"<Coordinates>\n";
+    result<<"\t<DataArray type=\"Float32\" Name=\"X\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
+    n++;
+    result<<"\t<DataArray type=\"Float32\" Name=\"Y\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
+    n++;
+    result<<"\t<DataArray type=\"Float32\" Name=\"Z\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
+    n++;
+    result<<"</Coordinates>\n";
+    result<<"</Piece>\n";
+    result<<"</RectilinearGrid>\n";
 }
 
 void vtr3D::endingParallel(std::ofstream &result, const char *A10, const int M10, const int num)
 {
-	result<<"<PCoordinates>\n";
+    result<<"<PCoordinates>\n";
     result<<"\t<PDataArray type=\"Float32\" Name=\"X\" format=\"appended\"/>\n";
-	result<<"\t<PDataArray type=\"Float32\" Name=\"Y\" format=\"appended\"/>\n";
-	result<<"\t<PDataArray type=\"Float32\" Name=\"Z\" format=\"appended\"/>\n";
+    result<<"\t<PDataArray type=\"Float32\" Name=\"Y\" format=\"appended\"/>\n";
+    result<<"\t<PDataArray type=\"Float32\" Name=\"Z\" format=\"appended\"/>\n";
     result<<"</PCoordinates>\n";
 
-	for(int n=0; n<M10; ++n)
-	{
-		sprintf(pname,"REEF3D-%s-%08i-%06i.vtr",A10,num,n+1);
-		sprintf(pextent,"%i %i %i %i %i %i",piextent[0+6*n],piextent[1+6*n],piextent[2+6*n],piextent[3+6*n],piextent[4+6*n],piextent[5+6*n]);
-		result<<"<Piece Extent=\""<<pextent<<"\" Source=\""<<pname<<"\"/>\n";
-	}
+    for(int n=0; n<M10; ++n)
+    {
+        sprintf(pname,"REEF3D-%s-%08i-%06i.vtr",A10,num,n+1);
+        sprintf(pextent,"%i %i %i %i %i %i",piextent[0+6*n],piextent[1+6*n],piextent[2+6*n],piextent[3+6*n],piextent[4+6*n],piextent[5+6*n]);
+        result<<"<Piece Extent=\""<<pextent<<"\" Source=\""<<pname<<"\"/>\n";
+    }
 
-	result<<"</PRectilinearGrid>\n";
-	result<<"</VTKFile>"<<flush;
+    result<<"</PRectilinearGrid>\n";
+    result<<"</VTKFile>"<<flush;
 }
 
 void vtr3D::extent(lexer *p, ghostcell *pgc)
 {
-	int iextent[6];
-	iextent[0]=p->origin_i;
-	iextent[1]=p->origin_i+p->knox;
-	iextent[2]=p->origin_j;
-	iextent[3]=p->origin_j+p->knoy;
-	iextent[4]=p->origin_k;
-	iextent[5]=p->origin_k+p->knoz;
+    int iextent[6];
+    iextent[0]=p->origin_i;
+    iextent[1]=p->origin_i+p->knox;
+    iextent[2]=p->origin_j;
+    iextent[3]=p->origin_j+p->knoy;
+    iextent[4]=p->origin_k;
+    iextent[5]=p->origin_k+p->knoz;
 
     if ( p->mpirank == 0)
     piextent = (int *)malloc(p->mpi_size*6*sizeof(int));
@@ -128,35 +128,35 @@ void vtr3D::extent(lexer *p, ghostcell *pgc)
 
 void vtr3D::structureWrite(lexer *p, fdm *a, std::ofstream &result)
 {
-	float ffn;
-	int iin;
+    float ffn;
+    int iin;
 
-	// Coordinates
-	// x
-	iin=4*(p->knox+1);
-	result.write((char*)&iin, sizeof (int));
+    // Coordinates
+    // x
+    iin=4*(p->knox+1);
+    result.write((char*)&iin, sizeof (int));
     ITLOOP
-	{
-		ffn=float(p->XN[IP]);
-		result.write((char*)&ffn, sizeof (float));
-	}
-	// y
-	iin=4*(p->knoy+1);
-	result.write((char*)&iin, sizeof (int));
+    {
+        ffn=float(p->XN[IP]);
+        result.write((char*)&ffn, sizeof (float));
+    }
+    // y
+    iin=4*(p->knoy+1);
+    result.write((char*)&iin, sizeof (int));
     JTLOOP
-	{
-		ffn=float(p->YN[JP]);
-		result.write((char*)&ffn, sizeof (float));
-	}
-	// z
-	iin=4*(p->knoz+1);
-	result.write((char*)&iin, sizeof (int));
+    {
+        ffn=float(p->YN[JP]);
+        result.write((char*)&ffn, sizeof (float));
+    }
+    // z
+    iin=4*(p->knoz+1);
+    result.write((char*)&iin, sizeof (int));
     KTLOOP
-	{
-		ffn=float(p->ZN[KP]);
-		result.write((char*)&ffn, sizeof (float));
-	}
+    {
+        ffn=float(p->ZN[KP]);
+        result.write((char*)&ffn, sizeof (float));
+    }
 
-	result<<"\n"<<"</AppendedData>\n";
+    result<<"\n"<<"</AppendedData>\n";
     result<<"</VTKFile>"<<flush;
 }
--- src/vtu3D.cpp
+++ src/vtu3D.cpp
@@ -37,45 +37,45 @@ vtu3D::~vtu3D()
 
 void vtu3D::folder(const char *A10)
 {
-	char name[30];
-	sprintf(name,"./REEF3D_%s_VTU", A10);
-	mkdir(name,0777);
+    char name[30];
+    sprintf(name,"./REEF3D_%s_VTU", A10);
+    mkdir(name,0777);
 }
 
 void vtu3D::offset(lexer *p, int *offset, int &n)
 {
-	// Points
+    // Points
     offset[n]=offset[n-1]+4*(p->pointnum)*3+4;
     ++n;
 
-	// Cells
+    // Cells
     offset[n]=offset[n-1] + 4*(p->tpcellnum*8) + 4;
     ++n;
     offset[n]=offset[n-1] + 4*(p->tpcellnum) + 4;
     ++n;
-	offset[n]=offset[n-1] + 4*(p->tpcellnum) + 4;
+    offset[n]=offset[n-1] + 4*(p->tpcellnum) + 4;
     ++n;
 }
 
 void vtu3D::beginning(lexer *p, std::ofstream &result)
 {
-	xmlVersion(result);
-	result<<"<VTKFile type=\"UnstructuredGrid\" ";
-	vtkVersion(result);
-	result<<"<UnstructuredGrid>\n";
+    xmlVersion(result);
+    result<<"<VTKFile type=\"UnstructuredGrid\" ";
+    vtkVersion(result);
+    result<<"<UnstructuredGrid>\n";
     if(p->P16==1)
     {
         timeValue(result,p->simtime);
     }
-	result<<"<Piece NumberOfPoints=\""<<p->pointnum<<"\" NumberOfCells=\""<<p->tpcellnum<<"\">\n";
+    result<<"<Piece NumberOfPoints=\""<<p->pointnum<<"\" NumberOfCells=\""<<p->tpcellnum<<"\">\n";
 }
 
 void vtu3D::beginningParallel(lexer *p, std::ofstream &result)
 {
-	xmlVersion(result);
-	result<<"<VTKFile type=\"PUnstructuredGrid\" ";
-	vtkVersion(result);
-	result<<"<PUnstructuredGrid GhostLevel=\"0\">\n";
+    xmlVersion(result);
+    result<<"<VTKFile type=\"PUnstructuredGrid\" ";
+    vtkVersion(result);
+    result<<"<PUnstructuredGrid GhostLevel=\"0\">\n";
     if(p->P16==1)
     {
         timeValue(result,p->simtime);
@@ -92,11 +92,11 @@ void vtu3D::ending(std::ofstream &result, const int *offset, int &n)
     result<<"<Cells>\n";
     result<<"\t<DataArray type=\"Int32\"  Name=\"connectivity\"  format=\"appended\" offset=\""<<offset[n]<<"\" />\n";
     ++n;
-	result<<"\t<DataArray type=\"Int32\"  Name=\"offsets\"  format=\"appended\" offset=\""<<offset[n]<<"\" />\n";
-	++n;
+    result<<"\t<DataArray type=\"Int32\"  Name=\"offsets\"  format=\"appended\" offset=\""<<offset[n]<<"\" />\n";
+    ++n;
     result<<"\t<DataArray type=\"Int32\"  Name=\"types\"  format=\"appended\" offset=\""<<offset[n]<<"\" />\n";
     ++n;
-	result<<"</Cells>\n";
+    result<<"</Cells>\n";
 
     result<<"</Piece>\n";
     result<<"</UnstructuredGrid>\n";
@@ -104,255 +104,255 @@ void vtu3D::ending(std::ofstream &result, const int *offset, int &n)
 
 void vtu3D::endingParallel(std::ofstream &result, const char *A10, const int M10, const int num)
 {
-	result<<"<PPoints>\n";
-	result<<"\t<PDataArray type=\"Float32\" NumberOfComponents=\"3\"/>\n";
-	result<<"</PPoints>\n";
+    result<<"<PPoints>\n";
+    result<<"\t<PDataArray type=\"Float32\" NumberOfComponents=\"3\"/>\n";
+    result<<"</PPoints>\n";
 
-	result<<"<Cells>\n";
-	result<<"\t<DataArray type=\"Int32\"  Name=\"connectivity\"/>\n";
-	result<<"\t<DataArray type=\"Int32\"  Name=\"offsets\" />\n";
-	result<<"\t<DataArray type=\"Int32\"  Name=\"types\" />\n";
-	result<<"</Cells>\n";
+    result<<"<Cells>\n";
+    result<<"\t<DataArray type=\"Int32\"  Name=\"connectivity\"/>\n";
+    result<<"\t<DataArray type=\"Int32\"  Name=\"offsets\" />\n";
+    result<<"\t<DataArray type=\"Int32\"  Name=\"types\" />\n";
+    result<<"</Cells>\n";
 
-	for(int n=0; n<M10; ++n)
-	{
-	sprintf(pname,"REEF3D-%s-%08i-%06i.vtu",A10,num,n+1);
-	result<<"<Piece Source=\""<<pname<<"\"/>\n";
-	}
+    for(int n=0; n<M10; ++n)
+    {
+    sprintf(pname,"REEF3D-%s-%08i-%06i.vtu",A10,num,n+1);
+    result<<"<Piece Source=\""<<pname<<"\"/>\n";
+    }
 
-	result<<"</PUnstructuredGrid>\n";
-	result<<"</VTKFile>"<<flush;
+    result<<"</PUnstructuredGrid>\n";
+    result<<"</VTKFile>"<<flush;
 }
 
 void vtu3D::structureWrite(lexer *p, fdm *a, std::ofstream &result)
 {
-	float ffn;
-	int iin;
-	double phase=0.0;
-	double zcoor;
+    float ffn;
+    int iin;
+    double phase=0.0;
+    double zcoor;
 
-	//  XYZ
-	double theta_y = p->B192_1*(PI/180.0);
-	double omega_y = 2.0*PI*p->B192_2;
+    //  XYZ
+    double theta_y = p->B192_1*(PI/180.0);
+    double omega_y = 2.0*PI*p->B192_2;
 
-	if(p->B192==1 && p->simtime>=p->B194_s && p->simtime<=p->B194_e)
-	phase = omega_y*p->simtime;
+    if(p->B192==1 && p->simtime>=p->B194_s && p->simtime<=p->B194_e)
+    phase = omega_y*p->simtime;
 
-	iin=4*(p->pointnum)*3;
-	result.write((char*)&iin, sizeof (int));
-	TPLOOP
-	{
+    iin=4*(p->pointnum)*3;
+    result.write((char*)&iin, sizeof (int));
+    TPLOOP
+    {
 
-		zcoor=p->ZN[KP1];
+        zcoor=p->ZN[KP1];
 
-		ffn=float( (p->XN[IP1]-p->B192_3)*cos(theta_y*sin(phase)) - (zcoor-p->B192_4)*sin(theta_y*sin(phase)) + p->B192_3 
-					+ p->B181_1*sin((2.0*PI*p->B181_2)*p->simtime + p->B181_3));
-		result.write((char*)&ffn, sizeof (float));
+        ffn=float( (p->XN[IP1]-p->B192_3)*cos(theta_y*sin(phase)) - (zcoor-p->B192_4)*sin(theta_y*sin(phase)) + p->B192_3 
+                    + p->B181_1*sin((2.0*PI*p->B181_2)*p->simtime + p->B181_3));
+        result.write((char*)&ffn, sizeof (float));
 
-		ffn=float(p->YN[JP1]) + p->B182_1*std::sin((2.0*PI*p->B182_2)*p->simtime + p->B182_3);
-		result.write((char*)&ffn, sizeof (float));
-		
+        ffn=float(p->YN[JP1]) + p->B182_1*std::sin((2.0*PI*p->B182_2)*p->simtime + p->B182_3);
+        result.write((char*)&ffn, sizeof (float));
+        
 
-		ffn=float((p->XN[IP1]-p->B192_3)*sin(theta_y*sin(phase)) + (zcoor-p->B192_4)*cos(theta_y*sin(phase)) + p->B192_4
-					+ p->B183_1*sin((2.0*PI*p->B183_2)*p->simtime + p->B183_3));
-		result.write((char*)&ffn, sizeof (float));
-	}
-	//  Connectivity
+        ffn=float((p->XN[IP1]-p->B192_3)*sin(theta_y*sin(phase)) + (zcoor-p->B192_4)*cos(theta_y*sin(phase)) + p->B192_4
+                    + p->B183_1*sin((2.0*PI*p->B183_2)*p->simtime + p->B183_3));
+        result.write((char*)&ffn, sizeof (float));
+    }
+    //  Connectivity
     iin=4*(p->tpcellnum)*8;
     result.write((char*)&iin, sizeof (int));
     BASEREVLOOP
     if(p->flag5[IJK]!=-20 && p->flag5[IJK]!=-30)
-	{
-		iin=int(a->nodeval(i-1,j-1,k-1)-1);
-		result.write((char*)&iin, sizeof (int));
+    {
+        iin=int(a->nodeval(i-1,j-1,k-1)-1);
+        result.write((char*)&iin, sizeof (int));
 
-		iin=int(a->nodeval(i,j-1,k-1))-1;
-		result.write((char*)&iin, sizeof (int));
+        iin=int(a->nodeval(i,j-1,k-1))-1;
+        result.write((char*)&iin, sizeof (int));
 
-		iin= int(a->nodeval(i,j,k-1))-1;
-		result.write((char*)&iin, sizeof (int));
+        iin= int(a->nodeval(i,j,k-1))-1;
+        result.write((char*)&iin, sizeof (int));
 
-		iin=int(a->nodeval(i-1,j,k-1))-1;
-		result.write((char*)&iin, sizeof (int));
+        iin=int(a->nodeval(i-1,j,k-1))-1;
+        result.write((char*)&iin, sizeof (int));
 
-		iin=int(a->nodeval(i-1,j-1,k))-1;
-		result.write((char*)&iin, sizeof (int));
+        iin=int(a->nodeval(i-1,j-1,k))-1;
+        result.write((char*)&iin, sizeof (int));
 
-		iin=int(a->nodeval(i,j-1,k))-1;
-		result.write((char*)&iin, sizeof (int));
+        iin=int(a->nodeval(i,j-1,k))-1;
+        result.write((char*)&iin, sizeof (int));
 
-		iin=int(a->nodeval(i,j,k))-1;
-		result.write((char*)&iin, sizeof (int));
+        iin=int(a->nodeval(i,j,k))-1;
+        result.write((char*)&iin, sizeof (int));
 
-		iin=int(a->nodeval(i-1,j,k))-1;
-		result.write((char*)&iin, sizeof (int));
-	}
+        iin=int(a->nodeval(i-1,j,k))-1;
+        result.write((char*)&iin, sizeof (int));
+    }
 
-	structureWriteEnd(p,result);
+    structureWriteEnd(p,result);
 }
 
 void vtu3D::structureWrite(lexer *p, fdm_fnpf *c, std::ofstream &result)
 {
-	float ffn;
-	int iin;
-	double phase=0.0;
-	double zcoor;
+    float ffn;
+    int iin;
+    double phase=0.0;
+    double zcoor;
 
-	//  XYZ
-	double theta_y = p->B192_1*(PI/180.0);
-	double omega_y = 2.0*PI*p->B192_2;
-	double waterlevel;
+    //  XYZ
+    double theta_y = p->B192_1*(PI/180.0);
+    double omega_y = 2.0*PI*p->B192_2;
+    double waterlevel;
 
-	if(p->B192==1 && p->simtime>=p->B194_s && p->simtime<=p->B194_e)
-	phase = omega_y*p->simtime;
+    if(p->B192==1 && p->simtime>=p->B194_s && p->simtime<=p->B194_e)
+    phase = omega_y*p->simtime;
 
-	iin=4*(p->pointnum)*3;
-	result.write((char*)&iin, sizeof (int));
-	TPLOOP
-	{
-		waterlevel = p->sl_ipol4eta(p->wet,c->eta,c->bed)+p->wd - p->sl_ipol4(c->bed);
+    iin=4*(p->pointnum)*3;
+    result.write((char*)&iin, sizeof (int));
+    TPLOOP
+    {
+        waterlevel = p->sl_ipol4eta(p->wet,c->eta,c->bed)+p->wd - p->sl_ipol4(c->bed);
 
-		zcoor = p->ZN[KP1]*waterlevel + p->sl_ipol4(c->bed);
+        zcoor = p->ZN[KP1]*waterlevel + p->sl_ipol4(c->bed);
 
 
-		if(p->wet[IJ]==0)
-		zcoor=c->bed(i,j);
+        if(p->wet[IJ]==0)
+        zcoor=c->bed(i,j);
 
-		if(i+p->origin_i==-1 && j+p->origin_j==-1 && p->wet[(0-p->imin)*p->jmax + (0-p->jmin)]==1)
-		zcoor = p->ZN[KP1]*c->WL(i,j) + c->bed(i,j);
+        if(i+p->origin_i==-1 && j+p->origin_j==-1 && p->wet[(0-p->imin)*p->jmax + (0-p->jmin)]==1)
+        zcoor = p->ZN[KP1]*c->WL(i,j) + c->bed(i,j);
 
-		ffn=float((p->XN[IP1]-p->B192_3)*cos(theta_y*sin(phase)) - (zcoor-p->B192_4)*sin(theta_y*sin(phase)) + p->B192_3);
-		result.write((char*)&ffn, sizeof (float));
+        ffn=float((p->XN[IP1]-p->B192_3)*cos(theta_y*sin(phase)) - (zcoor-p->B192_4)*sin(theta_y*sin(phase)) + p->B192_3);
+        result.write((char*)&ffn, sizeof (float));
 
-		ffn=float(p->YN[JP1]);
-		result.write((char*)&ffn, sizeof (float));
+        ffn=float(p->YN[JP1]);
+        result.write((char*)&ffn, sizeof (float));
 
-		ffn=float((p->XN[IP1]-p->B192_3)*sin(theta_y*sin(phase)) + (zcoor-p->B192_4)*cos(theta_y*sin(phase)) + p->B192_4);
-		result.write((char*)&ffn, sizeof (float));
-	}
+        ffn=float((p->XN[IP1]-p->B192_3)*sin(theta_y*sin(phase)) + (zcoor-p->B192_4)*cos(theta_y*sin(phase)) + p->B192_4);
+        result.write((char*)&ffn, sizeof (float));
+    }
 
     //  Connectivity
-	iin=4*(p->tpcellnum)*8;
-	result.write((char*)&iin, sizeof (int));
-	BASELOOP
-	if(p->flag5[IJK]!=-20 && p->flag5[IJK]!=-30)
-	{
-		iin=int(c->nodeval(i-1,j-1,k-1)-1);
-		result.write((char*)&iin, sizeof (int));
+    iin=4*(p->tpcellnum)*8;
+    result.write((char*)&iin, sizeof (int));
+    BASELOOP
+    if(p->flag5[IJK]!=-20 && p->flag5[IJK]!=-30)
+    {
+        iin=int(c->nodeval(i-1,j-1,k-1)-1);
+        result.write((char*)&iin, sizeof (int));
 
-		iin=int(c->nodeval(i,j-1,k-1))-1;
-		result.write((char*)&iin, sizeof (int));
+        iin=int(c->nodeval(i,j-1,k-1))-1;
+        result.write((char*)&iin, sizeof (int));
 
-		iin= int(c->nodeval(i,j,k-1))-1;
-		result.write((char*)&iin, sizeof (int));
+        iin= int(c->nodeval(i,j,k-1))-1;
+        result.write((char*)&iin, sizeof (int));
 
-		iin=int(c->nodeval(i-1,j,k-1))-1;
-		result.write((char*)&iin, sizeof (int));
+        iin=int(c->nodeval(i-1,j,k-1))-1;
+        result.write((char*)&iin, sizeof (int));
 
-		iin=int(c->nodeval(i-1,j-1,k))-1;
-		result.write((char*)&iin, sizeof (int));
+        iin=int(c->nodeval(i-1,j-1,k))-1;
+        result.write((char*)&iin, sizeof (int));
 
-		iin=int(c->nodeval(i,j-1,k))-1;
-		result.write((char*)&iin, sizeof (int));
+        iin=int(c->nodeval(i,j-1,k))-1;
+        result.write((char*)&iin, sizeof (int));
 
-		iin=int(c->nodeval(i,j,k))-1;
-		result.write((char*)&iin, sizeof (int));
+        iin=int(c->nodeval(i,j,k))-1;
+        result.write((char*)&iin, sizeof (int));
 
-		iin=int(c->nodeval(i-1,j,k))-1;
-		result.write((char*)&iin, sizeof (int));
-	}
+        iin=int(c->nodeval(i-1,j,k))-1;
+        result.write((char*)&iin, sizeof (int));
+    }
 
-	structureWriteEnd(p,result);
+    structureWriteEnd(p,result);
 }
 
 void vtu3D::structureWrite(lexer *p, fdm_nhf *d, std::ofstream &result)
 {
-	float ffn;
-	int iin;
-	double phase=0.0;
-	double zcoor;
+    float ffn;
+    int iin;
+    double phase=0.0;
+    double zcoor;
 
-	//  XYZ
-	iin=4*(p->pointnum)*3;
-	result.write((char*)&iin, sizeof (int));
-	TPLOOP
-	{
-	zcoor = p->ZN[KP1]*p->sl_ipol4(d->WL) + p->sl_ipol4(d->bed);
+    //  XYZ
+    iin=4*(p->pointnum)*3;
+    result.write((char*)&iin, sizeof (int));
+    TPLOOP
+    {
+    zcoor = p->ZN[KP1]*p->sl_ipol4(d->WL) + p->sl_ipol4(d->bed);
 
-	if(p->wet[IJ]==0)
-	zcoor=p->sl_ipol4(d->bed);
-	
-	if(i+p->origin_i==-1 && j+p->origin_j==-1 && p->wet[(0-p->imin)*p->jmax + (0-p->jmin)]==1)
-	zcoor = p->ZN[KP1]*d->WL(i,j) + d->bed(i,j);
+    if(p->wet[IJ]==0)
+    zcoor=p->sl_ipol4(d->bed);
+    
+    if(i+p->origin_i==-1 && j+p->origin_j==-1 && p->wet[(0-p->imin)*p->jmax + (0-p->jmin)]==1)
+    zcoor = p->ZN[KP1]*d->WL(i,j) + d->bed(i,j);
 
-	// -- 
-	ffn=float(p->XN[IP1]);
-	result.write((char*)&ffn, sizeof (float));
+    // -- 
+    ffn=float(p->XN[IP1]);
+    result.write((char*)&ffn, sizeof (float));
 
-	ffn=float(p->YN[JP1]);
-	result.write((char*)&ffn, sizeof (float));
+    ffn=float(p->YN[JP1]);
+    result.write((char*)&ffn, sizeof (float));
 
-	ffn=float(zcoor);
-	result.write((char*)&ffn, sizeof (float));
-	}
-	
-	//  Connectivity
-	iin=4*(p->tpcellnum)*8;
-	result.write((char*)&iin, sizeof (int));
-	BASELOOP
-	if(p->flag5[IJK]!=-20 && p->flag5[IJK]!=-30)
-	{
-	iin=int(d->NODEVAL[Im1Jm1Km1])-1;
-	result.write((char*)&iin, sizeof (int));
+    ffn=float(zcoor);
+    result.write((char*)&ffn, sizeof (float));
+    }
+    
+    //  Connectivity
+    iin=4*(p->tpcellnum)*8;
+    result.write((char*)&iin, sizeof (int));
+    BASELOOP
+    if(p->flag5[IJK]!=-20 && p->flag5[IJK]!=-30)
+    {
+    iin=int(d->NODEVAL[Im1Jm1Km1])-1;
+    result.write((char*)&iin, sizeof (int));
 
-	iin=int(d->NODEVAL[IJm1Km1])-1;
-	result.write((char*)&iin, sizeof (int));
+    iin=int(d->NODEVAL[IJm1Km1])-1;
+    result.write((char*)&iin, sizeof (int));
 
-	iin= int(d->NODEVAL[IJKm1])-1;
-	result.write((char*)&iin, sizeof (int));
+    iin= int(d->NODEVAL[IJKm1])-1;
+    result.write((char*)&iin, sizeof (int));
 
-	iin=int(d->NODEVAL[Im1JKm1])-1;
-	result.write((char*)&iin, sizeof (int));
+    iin=int(d->NODEVAL[Im1JKm1])-1;
+    result.write((char*)&iin, sizeof (int));
 
-	iin=int(d->NODEVAL[Im1Jm1K])-1;
-	result.write((char*)&iin, sizeof (int));
+    iin=int(d->NODEVAL[Im1Jm1K])-1;
+    result.write((char*)&iin, sizeof (int));
 
-	iin=int(d->NODEVAL[IJm1K])-1;
-	result.write((char*)&iin, sizeof (int));
+    iin=int(d->NODEVAL[IJm1K])-1;
+    result.write((char*)&iin, sizeof (int));
 
-	iin=int(d->NODEVAL[IJK])-1;
-	result.write((char*)&iin, sizeof (int));
+    iin=int(d->NODEVAL[IJK])-1;
+    result.write((char*)&iin, sizeof (int));
 
-	iin=int(d->NODEVAL[Im1JK])-1;
-	result.write((char*)&iin, sizeof (int));
-	}
+    iin=int(d->NODEVAL[Im1JK])-1;
+    result.write((char*)&iin, sizeof (int));
+    }
 
-	structureWriteEnd(p,result);
+    structureWriteEnd(p,result);
 }
 
 void vtu3D::structureWriteEnd(lexer *p, std::ofstream &result)
 {
-	int iin;
+    int iin;
 
-	// Offset of Connectivity
+    // Offset of Connectivity
     iin=4*(p->tpcellnum);
     result.write((char*)&iin, sizeof (int));
-	for(n=0;n<p->tpcellnum;++n)
-	{
-		iin=(n+1)*8;
-		result.write((char*)&iin, sizeof (int));
-	}
+    for(n=0;n<p->tpcellnum;++n)
+    {
+        iin=(n+1)*8;
+        result.write((char*)&iin, sizeof (int));
+    }
 
-	//  Cell types
+    //  Cell types
     iin=4*(p->tpcellnum);
     result.write((char*)&iin, sizeof (int));
-	for(n=0;n<p->tpcellnum;++n)
-	{
-		iin=12;
-		result.write((char*)&iin, sizeof (int));
-	}
+    for(n=0;n<p->tpcellnum;++n)
+    {
+        iin=12;
+        result.write((char*)&iin, sizeof (int));
+    }
 
-	result<<"\n";
-	result<<"</AppendedData>\n";
+    result<<"\n";
+    result<<"</AppendedData>\n";
     result<<"</VTKFile>"<<flush;
 }
