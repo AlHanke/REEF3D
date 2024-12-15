e5385bf13774391096a4366410c2b97788fab106
Alexander Hanke <alexander.hanke@ntnu.no>
2024-12-15 10:35:13 +0100

    Added rudimentary vtp class

---
+++ src/vtp3D.h
@@ -0,0 +1,39 @@
+/*--------------------------------------------------------------------
+REEF3D
+Copyright 2008-2024 Hans Bihs
+
+This file is part of REEF3D.
+
+REEF3D is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------
+Author: Hans Bihs
+--------------------------------------------------------------------*/
+
+#ifndef VTP3D_H_
+#define VTP3D_H_
+
+#include "vtk3D.h"
+#include "increment.h"
+
+class lexer;
+
+class vtp3D : public vtk3D , increment
+{
+    protected:
+        void beginning(lexer*, std::stringstream&);
+        void beginning(lexer*, std::ofstream&);
+        void beginningParallel(lexer*, std::ofstream&);
+};
+
+#endif
