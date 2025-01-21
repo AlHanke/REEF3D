4d85965a2fbcd42c0bca43e6285860d3ae88cfc8
Alexander Hanke <alexander.hanke@ntnu.no>
2025-01-21 14:41:31 +0100

    Added input parameters for rheology_f::Mohr_Coulomb_and_Herschel_Bulkley

--- src/rheology_Mohr_Coulomb_and_Herschel_Bulkley.cpp
+++ src/rheology_Mohr_Coulomb_and_Herschel_Bulkley.cpp
@@ -30,36 +30,38 @@ Author: Alexander Hanke
 // https://doi.org/10.5194/gmd-9-2909-2016
 // Three phase viscosity - air, slurry and gravel
 // Air phase is covered in fluid_update_rheology.cpp
+// inputs: n=W98, tau_00 & C* & rho_2=W105, b & m_y=W106
 double rheology_f::Mohr_Coulomb_and_Herschel_Bulkley(lexer* p, fdm* a, ghostcell* pgc)
 {
+    // fines + suspension = slurry: Herschel–Bulkley
     double shear_rate = sqrt(2)*strainterm(p,a);
-    const double b = 0.3;
-    double n = 0; // calibration : shear thinning vs shear thickening
-    const double C = 0.0; // volumetric solid concentration: the volume of all solid particles including fine material relative to the volume of the debris-flow material including water m^3/m^3
-    const double tau_00 = 0.0; // calibration : grid resolution sensitivity of the shear rate
+    const double b = p->W106_b;
+    const double n = p->W98; // calibration : shear thinning vs shear thickening
+    const double C = p->W105_C_total; // volumetric solid concentration: the volume of all solid particles including fine material relative to the volume of the debris-flow material including water m^3/m^3
+    const double tau_00 = p->W105_tau_00; // calibration : grid resolution sensitivity of the shear rate
     double tau_0 = C<0.47?tau_00:tau_00*exp(5*(C-0.47));
-    const double C_kaolinite_chlorite = 0.0;
-    const double C_illite = 0.0;
-    const double C_montmorillonite = 0.0;
+    const double C_kaolinite_chlorite = p->W105_C_kaolinite_chlorite;
+    const double C_illite = p->W105_C_illite;
+    const double C_montmorillonite = p->W105_C_montmorillonite;
     double P_0 = C_kaolinite_chlorite + 1.3*C_illite + 1.7*C_montmorillonite;
     double P_1 = P_0>0.27 ? 0.7*P_0 : P_0;
     double tau_y = tau_0*C*C*exp(22*C*P_1);
     double k = b*tau_y; // d_max<0.4mm
     double mu_2 = k * pow(fabs(shear_rate),n-1) + tau_y*pow(fabs(shear_rate),-1);
-    const double mu_0 = 0.0;
-    mu_2 = std::min(mu_2,mu_0);
     const double rho_2 = 0.0;
     double ny_2 = mu_2/rho_2;
+    ny_2 = std::min(ny_2,p->W95);
 
-    const double mu_min = 0.0; // minimal dynamic viscosity
-    const double delta = 0.0; // internal friction angle
-    const double m_y = 0.2; // ]0,1]
+    // gravel: Coulomb viscoplastic
+    const double mu_min = p->W107_mu_min; // minimal dynamic viscosity
+    const double delta = p->W107_delta; // internal friction angle
+    const double m_y = p->W106_m_y; // ]0,1]
     double mu_3 = mu_min + a->press(i,j,k)*sin(delta)/shear_rate*(1-exp(-m_y*shear_rate));
-    // ToDo: limiter
-    const double rho_3 = 0.0;
+    mu_3 = std::min(mu_3,p->W107_mu_0); // ToDo: check limiter
+    const double rho_3 = p->W107_rho_3;
     double ny_3 = mu_3/rho_3;
 
-    double a_2 = 0.0;
-    double a_3 = 0.0;
+    double a_2 = p->W108_a_2;
+    double a_3 = 1.0-a_3;
     return a_2*ny_2+a_3*ny_3;
 }
