c3438e5eaae6b9e96ffb08ff3c45918ad8c15853
Alexander Hanke <alexander.hanke@ntnu.no>
2025-01-30 18:32:53 +0100

    Dummy implementations for Mohr-Coulomb+Herschel-Berkley and Mohr-Coulomb+Bigham rheologies

--- src/rheology_f.h
+++ src/rheology_f.h
@@ -49,6 +49,8 @@ private:
     field4 tau_x,tau_y,tau_z;
     
     double Herschel_Bulkley(lexer*,fdm*,ghostcell*);
+    double Mohr_Coulomb_and_Herschel_Bulkley(lexer*,fdm*,ghostcell*){return 0;};
+    double Mohr_Coulomb_and_Bingham(lexer*,fdm*,ghostcell*){return 0;};
     
 	
 	double gamma;
