96d323dd3b648eb5bfd66313b6ee712d48032d43
Alexander Hanke <alexander.hanke@ntnu.no>
2025-02-18 16:02:49 +0100

    Condensed assignments of density_rheo

--- src/momentum_FCC3.cpp
+++ src/momentum_FCC3.cpp
@@ -116,7 +116,7 @@ momentum_FCC3::momentum_FCC3(lexer *p, fdm *a, ghostcell *pgc, convection *pconv
 	pupdate = new fluid_update_void();
     
     // face density
-    if((p->F80==0) && p->H10==0 && p->W30==0  && p->F300==0 && p->W90==0 && (p->X10==0 || p->X10==1))
+    if(p->F80==0 && p->H10==0 && p->W30==0  && p->F300==0 && p->W90==0 && (p->X10==0 || p->X10==1))
     pd = new density_df(p);
     
 	if(p->F80==0 && p->H10==0 && p->W30==1  && p->F300==0 && p->W90==0)
@@ -131,10 +131,7 @@ momentum_FCC3::momentum_FCC3(lexer *p, fdm *a, ghostcell *pgc, convection *pconv
     if(p->F80>0 && p->H10==0 && p->W30==0  && p->F300==0 && p->W90==0)
 	pd = new density_vof(p);
     
-    if(p->F30>0 && p->H10==0 && p->W30==0  && p->F300==0 && p->W90>0)
-    pd = new density_rheo(p);
-    
-    if(p->F300>=1)
+    if((p->F30>0 && p->H10==0 && p->W30==0  && p->F300==0 && p->W90>0) || p->F300>=1)
     pd = new density_rheo(p);
 
 	if(p->F46==2)
