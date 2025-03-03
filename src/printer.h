332b7c05190185c031f0e834e4660035480a1f7e
Alexander Hanke <alexander.hanke@ntnu.no>
2025-03-03 18:35:53 +0100

    Reordered printer_CFD::start and removed solver as a parameter

--- src/printer.h
+++ src/printer.h
@@ -42,8 +42,8 @@ class printer
 {
 public:
     // CFD
-	virtual void start(fdm*,lexer*,ghostcell*,turbulence*,heat*,ioflow*,solver*,data*,concentration*,multiphase*,sediment*){};
-    virtual void print_stop(fdm*,lexer*,ghostcell*,turbulence*,heat*,ioflow*,solver*,data*,concentration*,multiphase*,sediment*){};
+    virtual void start(lexer*,fdm*,ghostcell*,turbulence*,heat*,ioflow*,data*,concentration*,multiphase*,sediment*){};
+    virtual void print_stop(lexer*,fdm*,ghostcell*,turbulence*,heat*,ioflow*,data*,concentration*,multiphase*,sediment*){};
     // NHFLOW
     virtual void start(lexer*,fdm_nhf*,ghostcell*,ioflow*,nhflow_turbulence*,sediment*){};
     virtual void print_stop(lexer*,fdm_nhf*,ghostcell*,ioflow*,nhflow_turbulence*,sediment*){};
