2b56aca3aa0c6d73140d95abeac6ec281ccf8186
Alexander Hanke <alexander.hanke@ntnu.no>
2025-03-11 12:41:06 +0100

    Cleaned up density assignements

--- src/pjm.cpp
+++ src/pjm.cpp
@@ -42,13 +42,13 @@ pjm::pjm(lexer* p, fdm *a, ghostcell *pgc, heat *&pheat, concentration *&ppconc)
 {
     pconc = ppconc;
     
-    if((p->F80==0) && p->H10==0 && p->W30==0  && p->F300==0 && p->W90==0 && p->X10==0)
+    if(p->F80==0 && p->H10==0 && p->W30==0 && p->F300==0 && p->W90==0 && p->X10==0)
     pd = new density_f(p);
     
-    if((p->F80==0) && p->H10==0 && p->W30==0  && p->F300==0 && p->W90==0 && p->X10==1)  
+    if(p->F80==0 && p->H10==0 && p->W30==0 && p->F300==0 && p->W90==0 && p->X10==1)  
     pd = new density_df(p);
     
-    if(p->F80==0 && p->H10==0 && p->W30==1  && p->F300==0 && p->W90==0)
+    if(p->F80==0 && p->H10==0 && p->W30==1 && p->F300==0 && p->W90==0)
     pd = new density_comp(p);
     
     if(p->F80==0 && p->H10>0 && p->F300==0 && p->W90==0)
@@ -57,10 +57,10 @@ pjm::pjm(lexer* p, fdm *a, ghostcell *pgc, heat *&pheat, concentration *&ppconc)
     if(p->F80==0 && p->C10>0 && p->F300==0 && p->W90==0)
     pd = new density_conc(p,pconc);
     
-    if(p->F80>0 && p->H10==0 && p->W30==0  && p->F300==0 && p->W90==0)
+    if(p->F80>0 && p->H10==0 && p->W30==0 && p->F300==0 && p->W90==0)
     pd = new density_vof(p);
     
-    if((p->F30>0 && p->H10==0 && p->W30==0  && p->F300==0 && p->W90>0) || p->F300>=1)
+    if((p->F30>0 && p->H10==0 && p->W30==0 && p->F300==0 && p->W90>0) || p->F300>=1)
     pd = new density_rheo(p);
     
     gcval_press=40;  
