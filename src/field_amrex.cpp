c86818e9f6a3eb5189c4372d434e0dc6719be694
Alexander Hanke <alexander.hanke@ntnu.no>
2026-02-15 18:25:49 +0100

    Renamed internal lexer pointer in field_amrex

--- src/field_amrex.cpp
+++ src/field_amrex.cpp
@@ -32,22 +32,22 @@ Author: Alexander Hanke
 
 field_amrex::field_amrex(lexer* p)
 {
-    pp = p;
+    field_amrex::p = p;
 }
 
 double& field_amrex::operator()(int ii, int jj, int kk) noexcept
 {
-    return (mf[pp->level][*(pp->amr_mfi)].array()(amrex::IntVect{AMREX_D_DECL(ii, jj, kk)} + amrex::IntVect{amrex::lbound(pp->amr_mfi->validbox())}, 0));
+    return (mf[p->level][*(p->amr_mfi)].array()(amrex::IntVect{AMREX_D_DECL(ii, jj, kk)} + amrex::IntVect{amrex::lbound(p->amr_mfi->validbox())}, 0));
 }
 
 void field_amrex::setVal(double val, bool includeGhost)
 {
-    mf[pp->level].setVal(val, includeGhost ? mf[pp->level].nGrowVect() : amrex::IntVect{0});
+    mf[p->level].setVal(val, includeGhost ? mf[p->level].nGrowVect() : amrex::IntVect{0});
 }
 
 void field_amrex::FillBoundary()
 {
-    mf[pp->level].FillBoundary(pp->amrex_geometry[pp->level].periodicity());
+    mf[p->level].FillBoundary(p->amrex_geometry[p->level].periodicity());
 }
 
 void field_amrex::FillDomainBoundary()
