f40d62d0943bbe57fc4ae0aacd560b316a24d3dd
Alexander Hanke <alexander.hanke@ntnu.no>
2026-02-15 18:33:16 +0100

    Updated FillDomainBoundary
    - Moved implementation to field{1-4}
    - Added template for BcDecision to MyExtBCFill
    - Added logic to use BcDecision
    - Added helper method in field_armex to simplify calling MyExtBCFill
    - Added placeholders for other boundary conditions

--- src/fields_amrex.h
+++ src/fields_amrex.h
@@ -31,6 +31,9 @@ class field1 : public field_amrex
 public:
     field1(lexer*);
     virtual ~field1() = default;
+    void FillDomainBoundary(int gcv) override;
+private:
+    amrex_bc_func::Field1BcDecision::Field1Params params;
 };
 
 class field2 : public field_amrex
@@ -38,6 +41,9 @@ class field2 : public field_amrex
 public:
     field2(lexer*);
     virtual ~field2() = default;
+    void FillDomainBoundary(int gcv) override;
+private:
+    amrex_bc_func::Field2BcDecision::Field2Params params;
 };
 
 class field3 : public field_amrex
@@ -45,6 +51,9 @@ class field3 : public field_amrex
 public:
     field3(lexer*);
     virtual ~field3() = default;
+    void FillDomainBoundary(int gcv) override;
+private:
+    amrex_bc_func::Field3BcDecision::Field3Params params;
 };
 
 class field4 : public field_amrex
@@ -52,6 +61,9 @@ class field4 : public field_amrex
 public:
     field4(lexer*);
     virtual ~field4() = default;
+    void FillDomainBoundary(int gcv) override;
+private:
+    amrex_bc_func::Field4BcDecision::Field4Params params;
 };
 
 #endif
