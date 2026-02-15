23b36d158cf67988b055e85cc9ee994f9b19f406
Alexander Hanke <alexander.hanke@ntnu.no>
2026-02-15 18:35:34 +0100

    Added j_dir lock to dgc Current dgc filled values in j!=0 in 2D mode.

--- src/grid_helper_dgc4.cpp
+++ src/grid_helper_dgc4.cpp
@@ -158,6 +158,7 @@ void grid_helper::fill_dgc4(lexer* p)
         //i-j
         if(p->flag4[Im1Jm1K]<0 && p->flag4[Im1JK]<0 && p->flag4[IJm1K]<0 && hgc[Im1Jm1K]==0)
         if((i>=0 || p->nb1<0) && (j>=0 || p->nb3<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
@@ -171,6 +172,7 @@ void grid_helper::fill_dgc4(lexer* p)
 
         if(p->flag4[Ip1Jm1K]<0 && p->flag4[Ip1JK]<0 && p->flag4[IJm1K]<0 && hgc[Ip1Jm1K]==0)
         if((i<p->knox || p->nb4<0) && (j>=0 || p->nb3<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
@@ -184,6 +186,7 @@ void grid_helper::fill_dgc4(lexer* p)
 
         if(p->flag4[Ip1Jp1K]<0 && p->flag4[Ip1JK]<0 && p->flag4[IJp1K]<0 && hgc[Ip1Jp1K]==0)
         if((i<p->knox || p->nb4<0) && (j<p->knoy || p->nb2<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
@@ -197,6 +200,7 @@ void grid_helper::fill_dgc4(lexer* p)
 
         if(p->flag4[Im1Jp1K]<0 && p->flag4[Im1JK]<0 && p->flag4[IJp1K]<0 && hgc[Im1Jp1K]==0)
         if((i>=0 || p->nb1<0) && (j<p->knoy || p->nb2<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
@@ -266,6 +270,7 @@ void grid_helper::fill_dgc4(lexer* p)
         // j-k
         if(p->flag4[IJp1Kp1]<0 && p->flag4[IJp1K]<0 && p->flag4[IJKp1]<0 && hgc[IJp1Kp1]==0)
         if((j<p->knoy || p->nb2<0) && (k<p->knoz || p->nb6<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
@@ -279,6 +284,7 @@ void grid_helper::fill_dgc4(lexer* p)
 
         if(p->flag4[IJm1Kp1]<0 && p->flag4[IJm1K]<0 && p->flag4[IJKp1]<0 && hgc[IJm1Kp1]==0)
         if((j>=0 || p->nb3<0) && (k<p->knoz || p->nb6<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
@@ -292,6 +298,7 @@ void grid_helper::fill_dgc4(lexer* p)
 
         if(p->flag4[IJp1Km1]<0 && p->flag4[IJp1K]<0 && p->flag4[IJKm1]<0 && hgc[IJp1Km1]==0)
         if((j<p->knoy || p->nb2<0) && (k>=0 || p->nb5<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
@@ -305,6 +312,7 @@ void grid_helper::fill_dgc4(lexer* p)
 
         if(p->flag4[IJm1Km1]<0 && p->flag4[IJm1K]<0 && p->flag4[IJKm1]<0 && hgc[IJm1Km1]==0)
         if((j>=0 || p->nb3<0) && (k>=0 || p->nb5<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
@@ -320,6 +328,7 @@ void grid_helper::fill_dgc4(lexer* p)
         // diag
         if(p->flag4[Im1Jm1Km1]<0 && p->flag4[Im1JK]<0 && p->flag4[IJm1K]<0 && p->flag4[IJKm1]<0 && hgc[Im1Jm1Km1]==0)
         if((i>=0 || p->nb1<0) && (j>=0 || p->nb3<0) && (k>=0 || p->nb5<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
@@ -333,6 +342,7 @@ void grid_helper::fill_dgc4(lexer* p)
 
         if(p->flag4[Ip1Jm1Km1]<0 && p->flag4[Ip1JK]<0 && p->flag4[IJm1K]<0 && p->flag4[IJKm1]<0 && hgc[Ip1Jm1Km1]==0)
         if((i<p->knox || p->nb4<0) && (j>=0 || p->nb3<0) && (k>=0 || p->nb5<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
@@ -346,6 +356,7 @@ void grid_helper::fill_dgc4(lexer* p)
 
         if(p->flag4[Ip1Jp1Km1]<0 && p->flag4[Ip1JK]<0 && p->flag4[IJp1K]<0 && p->flag4[IJKm1]<0 && hgc[Ip1Jp1Km1]==0)
         if((i<p->knox || p->nb4<0) && (j<p->knoy || p->nb2<0) && (k>=0 || p->nb5<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
@@ -359,6 +370,7 @@ void grid_helper::fill_dgc4(lexer* p)
 
         if(p->flag4[Im1Jp1Km1]<0 && p->flag4[Im1JK]<0 && p->flag4[IJp1K]<0 && p->flag4[IJKm1]<0 && hgc[Im1Jp1Km1]==0)
         if((i>=0 || p->nb1<0) && (j<p->knoy || p->nb2<0) && (k>=0 || p->nb5<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
@@ -372,6 +384,7 @@ void grid_helper::fill_dgc4(lexer* p)
 
         if(p->flag4[Im1Jm1Kp1]<0 && p->flag4[Im1JK]<0 && p->flag4[IJm1K]<0 && p->flag4[IJKp1]<0 && hgc[Im1Jm1Kp1]==0)
         if((i>=0 || p->nb1<0) && (j>=0 || p->nb3<0) && (k<p->knoz || p->nb6<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
@@ -385,6 +398,7 @@ void grid_helper::fill_dgc4(lexer* p)
 
         if(p->flag4[Ip1Jm1Kp1]<0 && p->flag4[Ip1JK]<0 && p->flag4[IJm1K]<0 && p->flag4[IJKp1]<0 && hgc[Ip1Jm1Kp1]==0)
         if((i<p->knox || p->nb4<0) && (j>=0 || p->nb3<0) && (k<p->knoz || p->nb6<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
@@ -398,6 +412,7 @@ void grid_helper::fill_dgc4(lexer* p)
 
         if(p->flag4[Ip1Jp1Kp1]<0 && p->flag4[Ip1JK]<0 && p->flag4[IJp1K]<0 && p->flag4[IJKp1]<0 && hgc[Ip1Jp1Kp1]==0)
         if((i<p->knox || p->nb4<0) && (j<p->knoy || p->nb2<0) && (k<p->knoz || p->nb6<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
@@ -411,6 +426,7 @@ void grid_helper::fill_dgc4(lexer* p)
 
         if(p->flag4[Im1Jp1Kp1]<0 && p->flag4[Im1JK]<0 && p->flag4[IJp1K]<0 && p->flag4[IJKp1]<0 && hgc[Im1Jp1Kp1]==0)
         if((i>=0 || p->nb1<0) && (j<p->knoy || p->nb2<0) && (k<p->knoz || p->nb6<0))
+        if(p->j_dir==1)
         {
             p->dgc4[count][0]=i;
             p->dgc4[count][1]=j;
