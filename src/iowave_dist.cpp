815358205ca0397639e0b3d20e620983dc7a8464
Alexander Hanke <alexander.hanke@ntnu.no>
2026-02-15 18:43:15 +0100

    IOWave formatting

--- src/iowave_dist.cpp
+++ src/iowave_dist.cpp
@@ -89,7 +89,7 @@ double iowave::ygen(lexer *p)
 	y1 = p->pos_y();
 	
 	dist = fabs(x1 - tan_alpha*y1 + tan_alpha*y0 - x0)/sqrt(pow(tan_alpha,2.0)+1.0);
-    
+
 	return dist;
 }
 
@@ -131,30 +131,29 @@ double iowave::distgen(lexer *p)
 {
     double x0,y0,denom;
 	double dist=1.0e20;
-    int test1,test2;    
-    
+    int test1,test2;
+
     x0 = p->pos_x();
     y0 = p->pos_y();
-    
+
     for(int qn=0;qn<p->B108;++qn)
     {
-    test1=0;
-    test2=0;
-    
-    test1=intriangle(p,G1[qn][0],G1[qn][1],G3[qn][0],G3[qn][1],G2[qn][0],G2[qn][1],x0,y0);
-    test2=intriangle(p,G3[qn][0],G3[qn][1],G4[qn][0],G4[qn][1],G2[qn][0],G2[qn][1],x0,y0);
+        test1=0;
+        test2=0;
+
+        test1=intriangle(p,G1[qn][0],G1[qn][1],G3[qn][0],G3[qn][1],G2[qn][0],G2[qn][1],x0,y0);
+        test2=intriangle(p,G3[qn][0],G3[qn][1],G4[qn][0],G4[qn][1],G2[qn][0],G2[qn][1],x0,y0);
 
         if(test1==1||test2==1)
         {
-        denom = sqrt(pow(Ge[qn][1]-Gs[qn][1],2.0) + pow(Ge[qn][0]-Gs[qn][0],2.0));
-        denom = denom>1.0e-20?denom:1.0e20;
-        
-        dist = MIN(fabs((Ge[qn][1]-Gs[qn][1])*x0 - (Ge[qn][0]-Gs[qn][0])*y0 
-                  + Ge[qn][0]*Gs[qn][1] - Ge[qn][1]*Gs[qn][0])/denom,dist);
-        
+            denom = sqrt(pow(Ge[qn][1]-Gs[qn][1],2.0) + pow(Ge[qn][0]-Gs[qn][0],2.0));
+            denom = denom>1.0e-20?denom:1.0e20;
+
+            dist = MIN(fabs((Ge[qn][1]-Gs[qn][1])*x0 - (Ge[qn][0]-Gs[qn][0])*y0
+                    + Ge[qn][0]*Gs[qn][1] - Ge[qn][1]*Gs[qn][0])/denom,dist);
         }
     }
-    
+
 	return dist;
 }
 
@@ -162,30 +161,28 @@ double iowave::distbeach(lexer *p)
 {
     double x0,y0,denom;
 	double dist=1.0e20;
-    int test1,test2;    
-    
+    int test1,test2;
+
     x0 = p->pos_x();
     y0 = p->pos_y();
-    
+
     for(int qn=0;qn<p->B107;++qn)
     {
-    test1=0;
-    test2=0;
-    
-    test1=intriangle(p,B1[qn][0],B1[qn][1],B3[qn][0],B3[qn][1],B2[qn][0],B2[qn][1],x0,y0);
-    test2=intriangle(p,B3[qn][0],B3[qn][1],B4[qn][0],B4[qn][1],B2[qn][0],B2[qn][1],x0,y0);
+        test1=0;
+        test2=0;
+
+        test1=intriangle(p,B1[qn][0],B1[qn][1],B3[qn][0],B3[qn][1],B2[qn][0],B2[qn][1],x0,y0);
+        test2=intriangle(p,B3[qn][0],B3[qn][1],B4[qn][0],B4[qn][1],B2[qn][0],B2[qn][1],x0,y0);
 
         if(test1==1||test2==1)
         {
-        denom = sqrt(pow(Be[qn][1]-Bs[qn][1],2.0) + pow(Be[qn][0]-Bs[qn][0],2.0));
-        denom = denom>1.0e-20?denom:1.0e20;
-        
-        dist = MIN(fabs((Be[qn][1]-Bs[qn][1])*x0 - (Be[qn][0]-Bs[qn][0])*y0 
-                  + Be[qn][0]*Bs[qn][1] - Be[qn][1]*Bs[qn][0])/denom,dist);
-        
+            denom = sqrt(pow(Be[qn][1]-Bs[qn][1],2.0) + pow(Be[qn][0]-Bs[qn][0],2.0));
+            denom = denom>1.0e-20?denom:1.0e20;
+
+            dist = MIN(fabs((Be[qn][1]-Bs[qn][1])*x0 - (Be[qn][0]-Bs[qn][0])*y0
+                    + Be[qn][0]*Bs[qn][1] - Be[qn][1]*Bs[qn][0])/denom,dist);
         }
     }
-    
+
 	return dist;
 }
-
