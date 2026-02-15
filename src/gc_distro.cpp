492a091658748098aee9182032232a6935cb9614
Alexander Hanke <alexander.hanke@ntnu.no>
2026-02-15 18:24:42 +0100

    Removed boundary condition application types
    - gc_atmo
    - gc_debug
    - dirichlet-para
    - gc_extend
    - gravity_press
    - lsm
    - sommerfeld (3D method only)
    - gc_nhpress

--- src/gc_distro.cpp
+++ src/gc_distro.cpp
@@ -35,12 +35,6 @@ void ghostcell::gcdistro(field &f, int ii, int jj, int kk, double dist, bc_label
         case bc_labels::DIRICHLET_ORTH:
             dirichlet_ortho(f,dist,cs);
             break;
-        case bc_labels::DIRICHLET_PARA:
-            dirichlet_para(f,dist,cs);
-            break;
-        case bc_labels::EXTEND:
-            extend(f,cs);
-            break;
         case bc_labels::NEUMANN:
             neumann(f,cs);
             break;
@@ -50,17 +44,8 @@ void ghostcell::gcdistro(field &f, int ii, int jj, int kk, double dist, bc_label
         case bc_labels::OUTFLOW:
             outflow(f,cs);
             break;
-        case bc_labels::SOMMERFELD:
-            sommerfeld(f,cs);
-            break;
         case bc_labels::POTENTIAL:
             potentialbc(f,cs);
-            break;
-        case bc_labels::NHPRESS:
-            nhpress(f,cs);
-            break;
-        case bc_labels::GRAVITY_PRESS:
-            gravity_press(f,cs);
             break;
         case bc_labels::DIRICHLET_ORTH_REFLECT:
             dirichlet_ortho_reflect(f,dist,cs);
@@ -68,17 +53,8 @@ void ghostcell::gcdistro(field &f, int ii, int jj, int kk, double dist, bc_label
         case bc_labels::DIRICHLET_PARA_REFLECT:
             dirichlet_para_reflect(f,dist,cs);
             break;
-        case bc_labels::ATMOSPHERE:
-            atmosphere(f);
-            break;
-        case bc_labels::LSM:
-            lsm(f,cs);
-            break;
         case bc_labels::HEATBC:
             heatbc(f,cs);
-            break;
-        case bc_labels::DEBUG:
-            gcb_debug(f,cs);
             break;
         default:
             break;
