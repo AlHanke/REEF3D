cc1518b1c1858ad60c5bac9e87fa10bb63c995b8
Alexander Hanke <alexander.hanke@ntnu.no>
2026-02-25 17:15:16 +0100

    Updated benchmark_vortex3D
    - Re-added phi
    - Formatting

--- src/benchmark_vortex3D.cpp
+++ src/benchmark_vortex3D.cpp
@@ -28,39 +28,56 @@ Author: Hans Bihs
 
 benchmark_vortex3D::benchmark_vortex3D(lexer *p, fdm *a)
 {
-    double r,xc,yc,zc,radius;
-    double H;
+    const double xc = 0.35;
+    const double yc = 0.35;
+	const double zc = 0.35;
+    const double radius = 0.15;
 
-    xc = 0.35;
-    yc = 0.35;
-	zc = 0.35;
-    radius = 0.15;
-
+    a->phi.setVal(-1.0);
     a->vof.setVal(0.0);
+
+    const double inverted = p->F151==1 ? -1.0 : 1.0;
 
 	LOOP
 	{
-    r = sqrt( pow(p->pos_x()-xc,2.0) + pow(p->pos_y()-yc,2.0) + pow(p->pos_z()-zc,2.0));
-	if(r<=radius)
-	a->vof(i,j,k)=1.0;
-	}
+        const double dist = sqrt(pow(p->pos_x()-xc,2.0) + pow(p->pos_y()-yc,2.0) + pow(p->pos_z()-zc,2.0));
+        double sign = dist>radius ? -1.0 : 1.0; // Inside the radius is positive, outside is negative
 
-	
-	if(p->F151==1)
-	LOOP
-    a->phi(i,j,k)*=-1.0;
+        a->phi(i,j,k)=sign*dist*inverted;
+
+        if(sign==1.0)
+        {
+            a->vof(i,j,k) = 1.0;
+        }
+	}
 	
     LOOP
 	{
-		H=a->vof(i,j,k);
+        double H;
+        if(p->F80>0)
+        {
+		    H=a->vof(i,j,k);
+        }
+        else
+        {
+            const double epsilon = p->F45*p->DXM;
+            if(a->phi(i,j,k) >= epsilon)
+            {
+                H=1.0;
+            }
+            else if(a->phi(i,j,k) < -epsilon)
+            {
+                H=0.0;
+            }
+            else
+            {
+                H=0.5*(1.0 + a->phi(i,j,k)/epsilon + (1.0/PI)*sin((PI*a->phi(i,j,k))/epsilon));
+            }
+        }
 
 		a->ro(i,j,k)= p->W1*H + p->W3*(1.0-H);
 		a->visc(i,j,k)= p->W2*H + p->W4*(1.0-H);
 	}
-}
-
-benchmark_vortex3D::~benchmark_vortex3D()
-{
 }
 
 void benchmark_vortex3D::start(lexer* p, fdm *a, ghostcell *pgc, convection *pconvec )
@@ -69,29 +86,29 @@ void benchmark_vortex3D::start(lexer* p, fdm *a, ghostcell *pgc, convection *pco
 
     ULOOP
     {
-    xc = p->pos_x() + 0.5*p->DXM;
-    yc = p->pos_y();
-	zc = p->pos_z();
+        xc = p->pos_x() + 0.5*p->DXM;
+        yc = p->pos_y();
+        zc = p->pos_z();
 
-    a->u(i,j,k) = 2.0* pow(sin(PI*xc),2.0) * sin(2.0*PI*yc) * sin(2.0*PI*zc) * cos((PI*p->simtime)/3.0);
+        a->u(i,j,k) = 2.0* pow(sin(PI*xc),2.0) * sin(2.0*PI*yc) * sin(2.0*PI*zc) * cos((PI*p->simtime)/3.0);
     }
 
     VLOOP
     {
-    xc = p->pos_x();
-    yc = p->pos_y() + 0.5*p->DXM;
-	zc = p->pos_z();
+        xc = p->pos_x();
+        yc = p->pos_y() + 0.5*p->DXM;
+        zc = p->pos_z();
 
-    a->v(i,j,k) = -sin(2.0*PI*xc) * pow(sin(PI*yc),2.0) * sin(2.0*PI*zc) * cos((PI*p->simtime)/3.0);
+        a->v(i,j,k) = -sin(2.0*PI*xc) * pow(sin(PI*yc),2.0) * sin(2.0*PI*zc) * cos((PI*p->simtime)/3.0);
     }
 	
 	WLOOP
     {
-    xc = p->pos_x();
-    yc = p->pos_y();
-	zc = p->pos_z() + 0.5*p->DXM;
+        xc = p->pos_x();
+        yc = p->pos_y();
+        zc = p->pos_z() + 0.5*p->DXM;
 
-    a->w(i,j,k) = -sin(2.0*PI*xc) * sin(2.0*PI*yc) * pow(sin(PI*zc),2.0) * cos((PI*p->simtime)/3.0);
+        a->w(i,j,k) = -sin(2.0*PI*xc) * sin(2.0*PI*yc) * pow(sin(PI*zc),2.0) * cos((PI*p->simtime)/3.0);
     }
 
     pgc->start1(p,a->u,10);
