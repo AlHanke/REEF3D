35290dff84faa7a2f32ba16ca585aba7c70fdfad
Alexander Hanke <alexander.hanke@ntnu.no>
2026-03-23 19:54:53 +0100

    Formatted flux_face_CDS2_2D

--- src/flux_face_CDS2_2D.h
+++ src/flux_face_CDS2_2D.h
@@ -26,23 +26,19 @@ Author: Hans Bihs
 #include"flux.h"
 #include"increment.h"
 
-using namespace std;
-
 class flux_face_CDS2_2D : public flux, public increment
 {
 public:
 
-	flux_face_CDS2_2D (lexer *p);
-	virtual ~flux_face_CDS2_2D();
+	flux_face_CDS2_2D (lexer*);
+	virtual ~flux_face_CDS2_2D() = default;
 
-	void u_flux(fdm* a,int,field&,double&,double&) override;
-	void v_flux(fdm* a,int,field&,double&,double&) override;
-	void w_flux(fdm* a,int,field&,double&,double&) override;
-
+	void u_flux(fdm*,int,field&,double&,double&) override;
+	void v_flux(fdm*,int,field&,double&,double&) override;
+	void w_flux(fdm*,int,field&,double&,double&) override;
     
 private:
     lexer *p;
-
 };
 
 #endif
