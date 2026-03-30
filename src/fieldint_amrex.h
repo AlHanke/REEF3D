a1be1da0a1a210cc90a213c12c6f90c6d20d16f3
Alexander Hanke <alexander.hanke@ntnu.no>
2026-03-30 15:01:28 +0200

    Added chaching to fieldint_armex

--- src/fieldint_amrex.h
+++ src/fieldint_amrex.h
@@ -36,22 +36,26 @@ public:
 
     inline int& operator()(int ii, int jj, int kk) noexcept override final
     {
-        return (mf[p->level][*(p->amr_cell_mfi)].array()(amrex::IntVect(AMREX_D_DECL(ii, jj, kk)) + amrex::IntVect(amrex::IntVect(p->amr_tile_lo)), 0));
+        refresh_cache_if_needed();
+        return m_cached_arr4(ii + m_cached_ox, jj + m_cached_oy, kk + m_cached_oz, 0);
     }
 
     inline const int& operator()(int ii, int jj, int kk) const noexcept override final
     {
-        return (mf[p->level][*(p->amr_cell_mfi)].const_array()(amrex::IntVect(AMREX_D_DECL(ii, jj, kk)) + amrex::IntVect(amrex::IntVect(p->amr_tile_lo)), 0));
+        refresh_const_cache_if_needed();
+        return m_cached_const_arr4(ii + m_cached_const_ox, jj + m_cached_const_oy, kk + m_cached_const_oz, 0);
     }
 
     inline int& operator()(const amrex::IntVect& iv, int comp = 0) noexcept override final
     {
-        return (mf[p->level][*(p->amr_cell_mfi)].array()(iv, comp));
+        refresh_cache_if_needed();
+        return m_cached_arr4(iv, comp);
     }
 
     inline const int& operator()(const amrex::IntVect& iv, int comp = 0) const noexcept override final
     {
-        return (mf[p->level][*(p->amr_cell_mfi)].const_array()(iv, comp));
+        refresh_const_cache_if_needed();
+        return m_cached_const_arr4(iv, comp);
     }
 
     void setVal(int val, bool includeGhost = false) override final;
@@ -68,6 +72,74 @@ protected:
 
     lexer *p = nullptr;
     amrex::Vector<amrex::iMultiFab> mf = {};
+
+private:
+    /// Refreshes the Array4<int> cache when the current tile or level changes.
+    /// Tile bounds are pre-computed by TILE_LOOP via set_tile_mfi.
+    AMREX_FORCE_INLINE void refresh_cache_if_needed()
+    {
+        const int cur_lev = p->level;
+        const int cur_idx = p->amr_fab_mfi_idx;
+        const int cur_tile_index = p->amr_local_tile_idx;
+        if (cur_lev != m_cached_level || cur_idx != m_cached_mfi_idx)
+        {
+            m_cached_arr4    = mf[cur_lev][*(p->amr_cell_mfi)].array();
+            m_cached_ox      = p->amr_tile_lo.x;
+            m_cached_oy      = p->amr_tile_lo.y;
+            m_cached_oz      = p->amr_tile_lo.z;
+            m_cached_mfi_idx = cur_idx;
+            m_cached_level   = cur_lev;
+            m_cached_til_idx = cur_tile_index;
+        }
+        else if(cur_tile_index != m_cached_til_idx)
+        {
+            m_cached_ox      = p->amr_tile_lo.x;
+            m_cached_oy      = p->amr_tile_lo.y;
+            m_cached_oz      = p->amr_tile_lo.z;
+            m_cached_til_idx = cur_tile_index;
+        }
+    }
+
+    /// Refreshes the Array4<const int> cache when the current tile or level changes.
+    AMREX_FORCE_INLINE void refresh_const_cache_if_needed() const
+    {
+        const int cur_lev = p->level;
+        const int cur_idx = p->amr_fab_mfi_idx;
+        const int cur_tile_index = p->amr_local_tile_idx;
+        if (cur_lev != m_cached_const_level || cur_idx != m_cached_const_mfi_idx)
+        {
+            m_cached_const_arr4    = mf[cur_lev][*(p->amr_cell_mfi)].const_array();
+            m_cached_const_ox      = p->amr_tile_lo.x;
+            m_cached_const_oy      = p->amr_tile_lo.y;
+            m_cached_const_oz      = p->amr_tile_lo.z;
+            m_cached_const_mfi_idx = cur_idx;
+            m_cached_const_level   = cur_lev;
+            m_cached_const_til_idx = cur_tile_index;
+        }
+        else if(cur_tile_index != m_cached_const_til_idx)
+        {
+            m_cached_const_ox      = p->amr_tile_lo.x;
+            m_cached_const_oy      = p->amr_tile_lo.y;
+            m_cached_const_oz      = p->amr_tile_lo.z;
+            m_cached_const_til_idx = cur_tile_index;
+        }
+    }
+
+    amrex::Array4<int> m_cached_arr4 = {};
+    int m_cached_ox      = 0;
+    int m_cached_oy      = 0;
+    int m_cached_oz      = 0;
+    int m_cached_mfi_idx = -1;
+    int m_cached_level   = -1;
+    int m_cached_til_idx = -1;
+
+    mutable amrex::Array4<const int> m_cached_const_arr4 = {};
+    mutable int m_cached_const_ox      = 0;
+    mutable int m_cached_const_oy      = 0;
+    mutable int m_cached_const_oz      = 0;
+    mutable int m_cached_const_mfi_idx = -1;
+    mutable int m_cached_const_level   = -1;
+    mutable int m_cached_const_til_idx = -1;
 };
 
 #endif
