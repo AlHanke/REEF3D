8148113bf03a48e77ad97245f2177ba827e6b821
Alexander Hanke <alexander.hanke@ntnu.no>
2026-04-08 13:23:00 +0200

    Updated parameter order of momentum_forcing_start

--- src/momentum_FCC3.cpp
+++ src/momentum_FCC3.cpp
@@ -354,7 +354,7 @@ void momentum_FCC3::start(lexer *p, fdm *a, ghostcell *pgc, vrans *pvrans, sixdo
     
     pupdate->start(p,a,pgc,urk1,vrk1,wrk1);
     
-    momentum_forcing_start(a, p, pgc, p6dof, pfsi,
+    momentum_forcing_start(p, a, pgc, p6dof, pfsi,
                            urk1, vrk1, wrk1, fx, fy, fz, 0, 1.0, false);
     
     pflow->pressure_io(p,a,pgc);
@@ -557,7 +557,7 @@ void momentum_FCC3::start(lexer *p, fdm *a, ghostcell *pgc, vrans *pvrans, sixdo
     
     pupdate->start(p,a,pgc,urk2,vrk2,wrk2);
     
-    momentum_forcing_start(a, p, pgc, p6dof, pfsi,
+    momentum_forcing_start(p, a, pgc, p6dof, pfsi,
                            urk2, vrk2, wrk2, fx, fy, fz, 1, 0.25, false);
 
     pflow->pressure_io(p,a,pgc);
@@ -759,7 +759,7 @@ void momentum_FCC3::start(lexer *p, fdm *a, ghostcell *pgc, vrans *pvrans, sixdo
     
     pupdate->start(p,a,pgc,a->u,a->v,a->w);
     
-    momentum_forcing_start(a, p, pgc, p6dof, pfsi,
+    momentum_forcing_start(p, a, pgc, p6dof, pfsi,
                            a->u, a->v, a->w, fx, fy, fz, 2, 2.0/3.0, true);
 
 	pflow->pressure_io(p,a,pgc);
