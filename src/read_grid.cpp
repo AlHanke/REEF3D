dc82c868f8bfd5ac146af221bf7e9c236dc1e9b1
Alexander Hanke <alexander.hanke@ntnu.no>
2026-04-08 11:21:30 +0200

    Added hard AMReX level cap

--- src/read_grid.cpp
+++ src/read_grid.cpp
@@ -829,6 +829,7 @@ void lexer::read_grid()
                 amrex_refined_grid_coords[lev][box].second[2] = ddn;
             }
         }
+        if(nlevs>max_nlevs) nlevs=max_nlevs;
     }
     #endif
 
