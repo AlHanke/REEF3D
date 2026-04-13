d58d9ffcde6578c4b322b1efc57261cf8f1e8daf
Alexander Hanke <alexander.hanke@ntnu.no>
2026-04-13 17:24:28 +0200

    Added AMReX based fields supporting multiple levels

--- src/field_amrex.cpp
+++ src/field_amrex.cpp
@@ -23,9 +23,9 @@ Author: Alexander Hanke
 #if USE_AMREX
 #include "field_amrex.h"
 #include "lexer.h"
-#include <AMReX_MultiFab.H>
-#include <AMReX_Array4.H>
 #include <AMReX_BCUtil.H>
+#include <AMReX_MFIter.H>
+#include <AMReX_MultiFab.H>
 
 field_amrex::field_amrex(lexer* p)
 {
@@ -34,62 +34,47 @@ field_amrex::field_amrex(lexer* p)
 
 double& field_amrex::operator()(int ii, int jj, int kk) noexcept
 {
-    using namespace amrex;
-
-    IntVect cell_index{AMREX_D_DECL(ii + pp->origin_i, jj + pp->origin_j, kk + pp->origin_k)};
-
-    for (MFIter mfi(mf); mfi.isValid(); ++mfi)
-    {
-        // const Box& box = mfi.fabbox();
-        // FArrayBox& fab = mf[mfi];
-        // Array4<Real> const& a = fab.array();
-        // const auto cell_index = box.smallEnd() + IntVect{AMREX_D_DECL(ii + pp->margin, jj + pp->margin, kk + pp->margin)};
-        // if(cell_index != IntVect{AMREX_D_DECL(ii + pp->origin_i, jj + pp->origin_j, kk + pp->origin_k)})
-        //     Abort("field_amrex::operator(): indices don't match.");
-        // const auto hi = ubound(box);
-        // return a(cell_index, 0);
-        if (!mfi.fabbox().contains(cell_index)) continue;
-        Array4<Real> arr = mf.array(mfi);
-        return arr(cell_index, 0);
-    }
-
-    Abort("field_amrex::operator(): index outside owned boxes.");
+    return (mf[pp->level][*(pp->amr_mfi)].array()(amrex::IntVect{AMREX_D_DECL(ii, jj, kk)} + amrex::IntVect{amrex::lbound(pp->amr_mfi->validbox())}, 0));
 }
 
 void field_amrex::setVal(double val, bool includeGhost)
 {
-    mf.setVal(val, includeGhost ? mf.nGrowVect() : amrex::IntVect{0});
+    mf[pp->level].setVal(val, includeGhost ? mf[pp->level].nGrowVect() : amrex::IntVect{0});
 }
 
 void field_amrex::FillBoundary()
 {
-    mf.FillBoundary(pp->amrex_geometry.periodicity());
+    mf[pp->level].FillBoundary(pp->amrex_geometry[pp->level].periodicity());
 }
 
 void field_amrex::FillDomainBoundary()
 {
-    amrex::FillDomainBoundary(mf, pp->amrex_geometry, bc);
+    amrex::FillDomainBoundary(mf[pp->level], pp->amrex_geometry[pp->level], bc[pp->level]);
 }
 
 void field_amrex::initialize_bc()
 {
     using namespace amrex;
 
-    bc.resize(mf.n_comp);
-    for (int n = 0; n < mf.nComp(); ++n)
+    bc.resize(pp->nlevs);
+    for(pp->level=0; pp->level<pp->nlevs; ++pp->level)
     {
-        for (int idim = 0; idim < AMREX_SPACEDIM; ++idim)
+        bc[pp->level].resize(mf[pp->level].n_comp);
+        for (int n = 0; n < mf[pp->level].nComp(); ++n)
         {
-            if (pp->amrex_geometry.isPeriodic(idim))
+            for (int idim = 0; idim < AMREX_SPACEDIM; ++idim)
             {
-                bc[n].setLo(idim, BCType::int_dir); // interior
-                bc[n].setHi(idim, BCType::int_dir);
-            }
-            else
-            {
-                // ToDo: Fix this
-                bc[n].setLo(idim, BCType::bogus);
-                bc[n].setHi(idim, BCType::bogus);
+                if (pp->amrex_geometry[pp->level].isPeriodic(idim))
+                {
+                    bc[pp->level][n].setLo(idim, BCType::int_dir); // interior
+                    bc[pp->level][n].setHi(idim, BCType::int_dir);
+                }
+                else
+                {
+                    // ToDo: Fix this
+                    bc[pp->level][n].setLo(idim, BCType::bogus);
+                    bc[pp->level][n].setHi(idim, BCType::bogus);
+                }
             }
         }
     }
--- src/fieldints_amrex.cpp
+++ src/fieldints_amrex.cpp
@@ -23,38 +23,62 @@ Author: Alexander Hanke
 #if USE_AMREX
 #include"fieldints_amrex.h"
 #include"lexer.h"
-#include <AMReX_MultiFab.H>
+#include <AMReX_iMultiFab.H>
 #include <AMReX_BoxArray.H>
 
 fieldint1::fieldint1(lexer *p) : fieldint_amrex(p)
 {
-    mf.define(p->amrex_box_array, p->amrex_distribution_mapping, num_components, p->margin);
-    mf.setVal(0, 0, mf.n_comp, p->margin);
-    mf.setVal(0);
-    FillBoundary();
+    mf.resize(p->nlevs);
+    for(p->level=0; p->level<p->nlevs; ++p->level)
+    {
+        mf[p->level].define(p->amrex_box_array[p->level], p->amrex_distribution_mapping[p->level], num_components, p->margin);
+        mf[p->level].setVal(0, 0, mf[p->level].n_comp, p->margin);
+        mf[p->level].setVal(0);
+    }
+    initialize_bc();
+    for(p->level=0; p->level<p->nlevs; ++p->level)
+        FillBoundary();
 }
 
 fieldint2::fieldint2(lexer *p) : fieldint_amrex(p)
 {
-    mf.define(p->amrex_box_array, p->amrex_distribution_mapping, num_components, p->margin);
-    mf.setVal(0, 0, mf.n_comp, p->margin);
-    mf.setVal(0);
-    FillBoundary();
+    mf.resize(p->nlevs);
+    for(p->level=0; p->level<p->nlevs; ++p->level)
+    {
+        mf[p->level].define(p->amrex_box_array[p->level], p->amrex_distribution_mapping[p->level], num_components, p->margin);
+        mf[p->level].setVal(0, 0, mf[p->level].n_comp, p->margin);
+        mf[p->level].setVal(0);
+    }
+    initialize_bc();
+    for(p->level=0; p->level<p->nlevs; ++p->level)
+        FillBoundary();
 }
 
 fieldint3::fieldint3(lexer *p) : fieldint_amrex(p)
 {
-    mf.define(p->amrex_box_array, p->amrex_distribution_mapping, num_components, p->margin);
-    mf.setVal(0, 0, mf.n_comp, p->margin);
-    mf.setVal(0);
-    FillBoundary();
+    mf.resize(p->nlevs);
+    for(p->level=0; p->level<p->nlevs; ++p->level)
+    {
+        mf[p->level].define(p->amrex_box_array[p->level], p->amrex_distribution_mapping[p->level], num_components, p->margin);
+        mf[p->level].setVal(0, 0, mf[p->level].n_comp, p->margin);
+        mf[p->level].setVal(0);
+    }
+    initialize_bc();
+    for(p->level=0; p->level<p->nlevs; ++p->level)
+        FillBoundary();
 }
 
 fieldint4::fieldint4(lexer *p) : fieldint_amrex(p)
 {
-    mf.define(p->amrex_box_array, p->amrex_distribution_mapping, num_components, p->margin);
-    mf.setVal(0, 0, mf.n_comp, p->margin);
-    mf.setVal(0);
-    FillBoundary();
+    mf.resize(p->nlevs);
+    for(p->level=0; p->level<p->nlevs; ++p->level)
+    {
+        mf[p->level].define(p->amrex_box_array[p->level], p->amrex_distribution_mapping[p->level], num_components, p->margin);
+        mf[p->level].setVal(0, 0, mf[p->level].n_comp, p->margin);
+        mf[p->level].setVal(0);
+    }
+    initialize_bc();
+    for(p->level=0; p->level<p->nlevs; ++p->level)
+        FillBoundary();
 }
 #endif
