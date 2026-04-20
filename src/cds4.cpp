74f209383a32d64b6f7f599e389f3f427914fa21
Alexander Hanke <alexander.hanke@ntnu.no>
2026-04-20 14:11:34 +0200

    Updated convection classes to mirror weno3_flux changes - Cleaned up constructors/destructors - Optimized start methods for AMReX: if/else + FIELDLOOP - Changed aij method to used template parameters and only local members - Whitespace updates

--- src/cds4.cpp
+++ src/cds4.cpp
@@ -32,99 +32,97 @@ Author: Hans Bihs
 #include"flux_face_FOU_2D.h"
 #include"flux_face_FOU_vrans_2D.h"
 
-cds4::cds4 (lexer *p)
+cds4::cds4(lexer *p)
 {
     if(p->j_dir==0)
     {
-    if(p->B200==0)
-    {
-        if(p->D11==1)
-        pflux = new flux_face_FOU_2D(p);
-        
-        if(p->D11==2)
-        pflux = new flux_face_CDS2_2D;
+        if(p->B200>=1 || p->S10==2)
+        {
+            if(p->D11==1)
+            pflux = new flux_face_FOU_vrans_2D(p);
+
+            else if(p->D11==2)
+            pflux = new flux_face_CDS2_vrans_2D;
+        }
+        else
+        {
+            if(p->D11==1)
+            pflux = new flux_face_FOU_2D(p);
+
+            else if(p->D11==2)
+            pflux = new flux_face_CDS2_2D;
+        }
     }
-    
-    if(p->B200>=1 || p->S10==2)
+    else if(p->j_dir==1)
     {
-        if(p->D11==1)
-        pflux = new flux_face_FOU_vrans_2D(p);
-        
-        if(p->D11==2)
-        pflux = new flux_face_CDS2_vrans_2D;
-    }
-    }
-    
-    if(p->j_dir==1)
-    {
-    if(p->B200==0)
-    {
-        if(p->D11==1)
-        pflux = new flux_face_FOU(p);
-        
-        if(p->D11==2)
-        pflux = new flux_face_CDS2;
-    }
-    
-    if(p->B200>=1 || p->S10==2)
-    {
-        if(p->D11==1)
-        pflux = new flux_face_FOU_vrans(p);
-        
-        if(p->D11==2)
-        pflux = new flux_face_CDS2_vrans;
-    }
+        if(p->B200>=1 || p->S10==2)
+        {
+            if(p->D11==1)
+            pflux = new flux_face_FOU_vrans(p);
+
+            else if(p->D11==2)
+            pflux = new flux_face_CDS2_vrans;
+        }
+        else
+        {
+            if(p->D11==1)
+            pflux = new flux_face_FOU(p);
+
+            else if(p->D11==2)
+            pflux = new flux_face_CDS2;
+        }
     }
 }
 
-cds4::~cds4()
-{
-}
-
-void cds4::start(lexer* p, fdm* a, field& b, int ipol, field& uvel, field& vvel, field& wvel)
+inline void cds4::start(lexer* p, fdm* a, field& b, int ipol, field& uvel, field& vvel, field& wvel)
 {
     if(ipol==1)
-    ULOOP
-    a->F(i,j,k)+=aij(p,a,b,1,uvel,vvel,wvel);
-    
-    if(p->j_dir==1)
-    if(ipol==2)
-    VLOOP
-    a->G(i,j,k)+=aij(p,a,b,2,uvel,vvel,wvel);
-
-    if(ipol==3)
-    WLOOP
-    a->H(i,j,k)+=aij(p,a,b,3,uvel,vvel,wvel);
-
-    if(ipol==4)
-    LOOP
-    a->L(i,j,k)+=aij(p,a,b,4,uvel,vvel,wvel);
+    {
+        FIELDLOOP_INC_MEMBER(a,F,
+            FIELD_CONST_INC(b); FIELD_CONST_INC(uvel); FIELD_CONST_INC(vvel); FIELD_CONST_INC(wvel),
+            F(i,j,k)+=aij(p,a,b,1,uvel,vvel,wvel);
+        )
+    }
+    else if(ipol==2 && p->j_dir==1)
+    {
+        FIELDLOOP_INC_MEMBER(a,G,
+            FIELD_CONST_INC(b); FIELD_CONST_INC(uvel); FIELD_CONST_INC(vvel); FIELD_CONST_INC(wvel),
+            G(i,j,k)+=aij(p,a,b,2,uvel,vvel,wvel);
+        )
+    }
+    else if(ipol==3)
+    {
+        FIELDLOOP_INC_MEMBER(a,H,
+            FIELD_CONST_INC(b); FIELD_CONST_INC(uvel); FIELD_CONST_INC(vvel); FIELD_CONST_INC(wvel),
+            H(i,j,k)+=aij(p,a,b,3,uvel,vvel,wvel);
+        )
+    }
+    else if(ipol==4)
+    {
+        FIELDLOOP_INC_MEMBER(a,L,
+            FIELD_CONST_INC(b); FIELD_CONST_INC(uvel); FIELD_CONST_INC(vvel); FIELD_CONST_INC(wvel),
+            L(i,j,k)+=aij(p,a,b,4,uvel,vvel,wvel);
+        )
+    }
 }
 
-double cds4::aij(lexer* p,fdm* a,field& b,int ipol, field& uvel, field& vvel, field& wvel)
-{		
-		dx=dy=dz=0.0;
-		
-		pflux->u_flux(a,ipol,uvel,ivel1,ivel2);
-        pflux->v_flux(a,ipol,vvel,jvel1,jvel2);
-        pflux->w_flux(a,ipol,wvel,kvel1,kvel2);
-        
-		
-		dx= (ivel2*(27.0*b(i,j,k) + 27.0*b(i+1,j,k) - 3.0*b(i+2,j,k) - 3.0*b(i-1,j,k))  
-		 -  ivel1* (27.0*b(i,j,k) + 27.0*b(i-1,j,k) - 3.0*b(i-2,j,k) - 3.0*b(i+1,j,k)))/(48.0*p->DXM);
+template<typename GenericField>
+inline double cds4::aij(lexer* p, fdm* a, const GenericField& b, int ipol, const GenericField& uvel, const GenericField& vvel, const GenericField& wvel)
+{
+    double ivel1,ivel2,jvel1,jvel2,kvel1,kvel2;
+    pflux->u_flux(a,ipol,uvel,ivel1,ivel2);
+    pflux->v_flux(a,ipol,vvel,jvel1,jvel2);
+    pflux->w_flux(a,ipol,wvel,kvel1,kvel2);
 
-		
-		if(p->j_dir==1)
-		dy= (jvel2*(27.0*b(i,j,k) + 27.0*b(i,j+1,k) - 3.0*b(i,j+2,k) - 3.0*b(i,j-1,k))  
-		 -  jvel1* (27.0*b(i,j,k) + 27.0*b(i,j-1,k) - 3.0*b(i,j-2,k) - 3.0*b(i,j+1,k)))/(48.0*p->DXM);
+    const double dx = (ivel2*(27.0*b(i,j,k) + 27.0*b(i+1,j,k) - 3.0*b(i+2,j,k) - 3.0*b(i-1,j,k))
+                    -  ivel1*(27.0*b(i,j,k) + 27.0*b(i-1,j,k) - 3.0*b(i-2,j,k) - 3.0*b(i+1,j,k)))/(48.0*p->DXM);
 
+    const double dy = p->j_dir==1 ?
+        (jvel2*(27.0*b(i,j,k) + 27.0*b(i,j+1,k) - 3.0*b(i,j+2,k) - 3.0*b(i,j-1,k))
+        -  jvel1*(27.0*b(i,j,k) + 27.0*b(i,j-1,k) - 3.0*b(i,j-2,k) - 3.0*b(i,j+1,k)))/(48.0*p->DXM) : 0.0;
 
-		
-		dz= (kvel2*(27.0*b(i,j,k) + 27.0*b(i,j,k+1) - 3.0*b(i,j,k+2) - 3.0*b(i,j,k-1))  
-		 -  kvel1* (27.0*b(i,j,k) + 27.0*b(i,j,k-1) - 3.0*b(i,j,k-2) - 3.0*b(i,j,k+1)))/(48.0*p->DXM);
-		
-		L = -dx-dy-dz;
+    const double dz = (kvel2*(27.0*b(i,j,k) + 27.0*b(i,j,k+1) - 3.0*b(i,j,k+2) - 3.0*b(i,j,k-1))
+                    -  kvel1*(27.0*b(i,j,k) + 27.0*b(i,j,k-1) - 3.0*b(i,j,k-2) - 3.0*b(i,j,k+1)))/(48.0*p->DXM);
 
-		return L;
+    return -dx-dy-dz;
 }
-
