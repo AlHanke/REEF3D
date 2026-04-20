3f656bbb91bcf84c8c607e01e03db91b7403654e
Alexander Hanke <alexander.hanke@ntnu.no>
2026-04-20 14:11:34 +0200

    Updated convection classes to mirror weno3_flux changes
    - Cleaned up constructors/destructors
    - Optimized start methods for AMReX: if/else + FIELDLOOP
    - Changed aij method to used template parameters and only local members
    - Whitespace updates

--- src/cicsam.cpp
+++ src/cicsam.cpp
@@ -32,257 +32,210 @@ Author: Hans Bihs
 #include"flux_face_FOU_2D.h"
 #include"flux_face_FOU_vrans_2D.h"
 
-cicsam::cicsam (lexer *p) 
+cicsam::cicsam(lexer *p)
 {
     if(p->j_dir==0)
     {
-    if(p->B200==0)
-    {
-        if(p->D11==1)
-        pflux = new flux_face_FOU_2D(p);
-        
-        if(p->D11==2)
-        pflux = new flux_face_CDS2_2D;
+        if(p->B200>=1 || p->S10==2)
+        {
+            if(p->D11==1)
+            pflux = new flux_face_FOU_vrans_2D(p);
+
+            else if(p->D11==2)
+            pflux = new flux_face_CDS2_vrans_2D;
+        }
+        else
+        {
+            if(p->D11==1)
+            pflux = new flux_face_FOU_2D(p);
+
+            else if(p->D11==2)
+            pflux = new flux_face_CDS2_2D;
+        }
     }
-    
-    if(p->B200>=1 || p->S10==2)
+    else if(p->j_dir==1)
     {
-        if(p->D11==1)
-        pflux = new flux_face_FOU_vrans_2D(p);
-        
-        if(p->D11==2)
-        pflux = new flux_face_CDS2_vrans_2D;
-    }
-    }
-    
-    if(p->j_dir==1)
-    {
-    if(p->B200==0)
-    {
-        if(p->D11==1)
-        pflux = new flux_face_FOU(p);
-        
-        if(p->D11==2)
-        pflux = new flux_face_CDS2;
-    }
-    
-    if(p->B200>=1 || p->S10==2)
-    {
-        if(p->D11==1)
-        pflux = new flux_face_FOU_vrans(p);
-        
-        if(p->D11==2)
-        pflux = new flux_face_CDS2_vrans;
-    }
+        if(p->B200>=1 || p->S10==2)
+        {
+            if(p->D11==1)
+            pflux = new flux_face_FOU_vrans(p);
+
+            else if(p->D11==2)
+            pflux = new flux_face_CDS2_vrans;
+        }
+        else
+        {
+            if(p->D11==1)
+            pflux = new flux_face_FOU(p);
+
+            else if(p->D11==2)
+            pflux = new flux_face_CDS2;
+        }
     }
 }
 
-cicsam::~cicsam()
-{
-}
-
-void cicsam::start(lexer* p, fdm* a, field& b, int ipol, field& uvel, field& vvel, field& wvel)
+inline void cicsam::start(lexer* p, fdm* a, field& b, int ipol, field& uvel, field& vvel, field& wvel)
 {
     if(ipol==1)
-    ULOOP
-    a->F(i,j,k)+=aij(p,a,b,1,uvel,vvel,wvel);
+    {
+        FIELDLOOP_INC_MEMBER(a,F,
+            FIELD_CONST_INC(b); FIELD_CONST_INC(uvel); FIELD_CONST_INC(vvel); FIELD_CONST_INC(wvel),
+            F(i,j,k)+=aij(p,a,b,1,uvel,vvel,wvel);
+        )
+    }
+    else if(ipol==2 && p->j_dir==1)
+    {
+        FIELDLOOP_INC_MEMBER(a,G,
+            FIELD_CONST_INC(b); FIELD_CONST_INC(uvel); FIELD_CONST_INC(vvel); FIELD_CONST_INC(wvel),
+            G(i,j,k)+=aij(p,a,b,2,uvel,vvel,wvel);
+        )
+    }
+    else if(ipol==3)
+    {
+        FIELDLOOP_INC_MEMBER(a,H,
+            FIELD_CONST_INC(b); FIELD_CONST_INC(uvel); FIELD_CONST_INC(vvel); FIELD_CONST_INC(wvel),
+            H(i,j,k)+=aij(p,a,b,3,uvel,vvel,wvel);
+        )
+    }
+    else if(ipol==4)
+    {
+        FIELDLOOP_INC_MEMBER(a,L,
+            FIELD_CONST_INC(b); FIELD_CONST_INC(uvel); FIELD_CONST_INC(vvel); FIELD_CONST_INC(wvel),
+            L(i,j,k)+=aij(p,a,b,4,uvel,vvel,wvel);
+        )
+    }
+}
 
+template<typename GenericField>
+inline double cicsam::aij(lexer* p, fdm* a, const GenericField& b, int ipol, const GenericField& uvel, const GenericField& vvel, const GenericField& wvel)
+{
+    double ivel1,ivel2,jvel1,jvel2,kvel1,kvel2;
+
+    pflux->u_flux(a,ipol,uvel,ivel1,ivel2);
+    pflux->v_flux(a,ipol,vvel,jvel1,jvel2);
+    pflux->w_flux(a,ipol,wvel,kvel1,kvel2);
+
+    const double fx1 = cface(p,a,b,1,-1,ivel1);
+    const double fx2 = cface(p,a,b,1,0,ivel2);
+    const double dx = (ivel2*fx2 - ivel1*fx1)/(p->DXM);
+
+    double dy = 0.0;
     if(p->j_dir==1)
-    if(ipol==2)
-    VLOOP
-    a->G(i,j,k)+=aij(p,a,b,2,uvel,vvel,wvel);
+    {
+        const double fy1 = cface(p,a,b,2,-1,jvel1);
+        const double fy2 = cface(p,a,b,2,0,jvel2);
+        dy = (jvel2*fy2 - jvel1*fy1)/(p->DXM);
+    }
 
-    if(ipol==3)
-    WLOOP
-    a->H(i,j,k)+=aij(p,a,b,3,uvel,vvel,wvel);
+    const double fz1 = cface(p,a,b,3,-1,kvel1);
+    const double fz2 = cface(p,a,b,3,0,kvel2);
+    const double dz = (kvel2*fz2 - kvel1*fz1)/(p->DXM);
 
-    if(ipol==4)
-    LOOP
-    a->L(i,j,k)+=aij(p,a,b,4,uvel,vvel,wvel);
+    return -dx-dy-dz;
 }
 
-double cicsam::aij(lexer* p,fdm* a,field& b,int ipol, field& uvel, field& vvel, field& wvel)
+template<typename GenericField>
+double cicsam::cface(lexer *p, fdm *a, const GenericField& b, int dir, int pos, double uwind)
 {
-    ul=ur=vl=vr=wl=wr=dx=dy=dz=0.0;
-		
+    double cj_,cj_s,cj_ss;
+    double cc,cc_,cu,cd;
+    double umax,Co,costheta,beta;
+    double gradx,grady,gradz;
 
-        pflux->u_flux(a,ipol,uvel,ivel1,ivel2);
-        pflux->v_flux(a,ipol,vvel,jvel1,jvel2);
-        pflux->w_flux(a,ipol,wvel,kvel1,kvel2);
-        
-        
-        fx1 = cface(p,a,b,1,-1,ivel1);
-        fx2 = cface(p,a,b,1,0,ivel2);
-
-		dx= (ivel2*fx2 - ivel1*fx1)/(p->DXM);
-		
-		
-        if(p->j_dir==1)
+    if(dir==1)
+    {
+        if(uwind>=0.0)
         {
-		fy1 = cface(p,a,b,2,-1,jvel1);
-	    fy2 = cface(p,a,b,2,0,jvel2);
-
-		dy= (jvel2*fy2 - jvel1*fy1)/(p->DXM);
+            cc = b(i+0+pos,j,k);
+            cu = b(i-1+pos,j,k);
+            cd = b(i+1+pos,j,k);
+            umax = 0.5*(a->u(i+pos,j,k)+a->u(i-1+pos,j,k));
         }
-		
-		
-		
-		fz1 = cface(p,a,b,3,-1,kvel1);
-	    fz2 = cface(p,a,b,3,0,kvel2);
+        else
+        {
+            cc = b(i+1+pos,j,k);
+            cu = b(i+2+pos,j,k);
+            cd = b(i-0+pos,j,k);
+            umax = 0.5*(a->u(i+1-pos,j,k)+a->u(i-0-pos,j,k));
+        }
+    }
+    else if(dir==2)
+    {
+        if(uwind>=0.0)
+        {
+            cc = b(i,j+0+pos,k);
+            cu = b(i,j-1+pos,k);
+            cd = b(i,j+1+pos,k);
+            umax = 0.5*(a->v(i,j+pos,k)+a->v(i,j-1+pos,k));
+        }
+        else
+        {
+            cc = b(i,j+1+pos,k);
+            cu = b(i,j+2+pos,k);
+            cd = b(i,j-0+pos,k);
+            umax = 0.5*(a->v(i,j+1-pos,k)+a->v(i,j-0-pos,k));
+        }
+    }
+    else if(dir==3)
+    {
+        if(uwind>=0.0)
+        {
+            cc = b(i,j,k+0+pos);
+            cu = b(i,j,k-1+pos);
+            cd = b(i,j,k+1+pos);
+            umax = 0.5*(a->w(i,j,k+pos)+a->w(i,j,k-1+pos));
+        }
+        else
+        {
+            cc = b(i,j,k+1+pos);
+            cu = b(i,j,k+2+pos);
+            cd = b(i,j,k-0+pos);
+            umax = 0.5*(a->w(i,j,k+1-pos)+a->w(i,j,k-0-pos));
+        }
+    }
 
-		dz= (kvel2*fz2 - kvel1*fz1)/(p->DXM);
+    cc_ = (cc-cu)/(fabs(cd-cu)>1.0e-20?(cd-cu):1.0e20);
 
-		
-		L = -dx-dy-dz;
-
-		return L;
-}
-
-double cicsam::cface(lexer *p,fdm *a,field& b,int dir, int pos, double uwind)
-{
-	double cj,cj_,cj_s,cj_ss;
-	double cc,cc_,cu,cd;
-    double umax,Co,theta,costheta,gamma,beta;
-	double gradx,grady,gradz;
-	
-	
-	if(dir==1)
-	{
-		if(uwind>=0.0)
-		{
-		cc = b(i+pos,j,k);
-		
-		cu = b(i-1+pos,j,k);
-		
-		cd = b(i+1+pos,j,k);
-        
-        umax = 0.5*(a->u(i+pos,j,k)+a->u(i-1+pos,j,k));
-		}
-        
-        if(uwind<0.0)
-		{
-		cc = b(i+1+pos,j,k);
-		
-		cu = b(i+2+pos,j,k);
-		
-		cd = b(i-0+pos,j,k);
-        
-        umax = 0.5*(a->u(i+1-pos,j,k)+a->u(i-0-pos,j,k));
-		}
-	}
-	
-	if(dir==2)
-	{
-		if(uwind>=0.0)
-		{
-		cc = b(i,j+pos,k);
-		
-		cu = b(i,j-1+pos,k);
-		
-		cd = b(i,j+1+pos,k);
-        
-        umax = 0.5*(a->v(i,j+pos,k)+a->v(i,j-1+pos,k));
-		}
-        
-        if(uwind<0.0)
-		{
-		cc = b(i,j+1+pos,k);
-	
-		cu = b(i,j+2+pos,k);
-		
-		cd = b(i,j-0+pos,k);
-        
-        umax = 0.5*(a->v(i,j+1-pos,k)+a->v(i,j-0-pos,k));
-		}
-	}
-	
-	if(dir==3)
-	{
-		if(uwind>=0.0)
-		{
-		cc = b(i,j,k+pos);
-		
-		cu = b(i,j,k-1+pos);
-		
-		cd = b(i,j,k+1+pos);
-        
-        umax = 0.5*(a->w(i,j,k+pos)+a->w(i,j,k-1+pos));
-		}
-        
-        if(uwind<0.0)
-		{
-		cc = b(i,j,k+1+pos);
-		
-		cu = b(i,j,k+2+pos);
-		
-		cd = b(i,j,k-0+pos);
-        
-        umax = 0.5*(a->w(i,j,k+1-pos)+a->w(i,j,k-0-pos));
-		}
-	}
-	
-	
-	cc_ = (cc-cu)/(fabs(cd-cu)>1.0e-20?(cd-cu):1.0e20); 
-    
-    
     Co = fabs(umax*p->dt/p->DXM);
-    
-    
+
     if(cc_>=0.0 && cc_<1.0)
-    cj_ = MIN(cc_/(fabs(Co)>1.0e-20?Co:1.0e20),1.0);
-    
-    if(cc_<0.0 || cc_>=1.0)
+    cj_ = std::min(cc_/(fabs(Co)>1.0e-20?Co:1.0e20),1.0);
+    else
     cj_ = cc_;
-    
-    
+
     if(cc_>=0.0 && cc_<1.0)
-    cj_s = MIN( (8.0*Co*cc_ + (1.0-Co)*(6.0*cc_+3.0))/8.0 , cj_);
-    
-    if(cc_<0.0 || cc_>=1.0)
+    cj_s = std::min((8.0*Co*cc_ + (1.0-Co)*(6.0*cc_+3.0))/8.0 , cj_);
+    else
     cj_s = cc_;
-	
-	
-	
-	if(uwind>=0.0)
-	{
-	gradx = fabs((b(i+1+pos,j,k)-b(i-1+pos,j,k))/(2.0*p->DXM));
-	grady = fabs((b(i,j+1+pos,k)-b(i,j-1+pos,k))/(2.0*p->DXM));
-	gradz = fabs((b(i,j,k+1+pos)-b(i,j,k-1+pos))/(2.0*p->DXM));
-	}
-	
-	if(uwind<0.0)
-	{
-	gradx = fabs((b(i+1+1+pos,j,k)-b(i-1+1+pos,j,k))/(2.0*p->DXM));
-	grady = fabs((b(i,j+1+1+pos,k)-b(i,j-1+1+pos,k))/(2.0*p->DXM));
-	gradz = fabs((b(i,j,k+1+1+pos)-b(i,j,k-1+1+pos))/(2.0*p->DXM));
-	}
-	
-	vl = sqrt(gradx*gradx + grady*grady + gradz*gradz);
-    
 
-	if(dir==1)
-	theta = acos(gradx/(vl>1.0e-20?vl:1.0e20));
+    if(uwind>=0.0)
+    {
+        gradx = fabs((b(i+1+pos,j,k)-b(i-1+pos,j,k))/(2.0*p->DXM));
+        grady = fabs((b(i,j+1+pos,k)-b(i,j-1+pos,k))/(2.0*p->DXM));
+        gradz = fabs((b(i,j,k+1+pos)-b(i,j,k-1+pos))/(2.0*p->DXM));
+    }
+    else
+    {
+        gradx = fabs((b(i+1+1+pos,j,k)-b(i-1+1+pos,j,k))/(2.0*p->DXM));
+        grady = fabs((b(i,j+1+1+pos,k)-b(i,j-1+1+pos,k))/(2.0*p->DXM));
+        gradz = fabs((b(i,j,k+1+1+pos)-b(i,j,k-1+1+pos))/(2.0*p->DXM));
+    }
 
-	if(dir==2)
-	theta = acos(grady/(vl>1.0e-20?vl:1.0e20));
-	
-	if(dir==3)
-	theta = acos(gradz/(vl>1.0e-20?vl:1.0e20));
-	
-	
-    
-    gamma = MIN((cos(2.0*theta)+1.0)/2.0,1.0);
-    
-    
-	
+    const double vl = sqrt(gradx*gradx + grady*grady + gradz*gradz);
+
+    double theta = 0.0;
+    if(dir==1)
+    theta = acos(gradx/(vl>1.0e-20?vl:1.0e20));
+    else if(dir==2)
+    theta = acos(grady/(vl>1.0e-20?vl:1.0e20));
+    else if(dir==3)
+    theta = acos(gradz/(vl>1.0e-20?vl:1.0e20));
+
+    const double gamma = std::min((cos(2.0*theta)+1.0)/2.0,1.0);
+
     cj_ss = cj_*gamma + cj_s*(1.0-gamma);
-    
-    
+
     beta = (cj_ss-cc_)/(fabs(1.0-cc_)>1.0e-20?(1.0-cc_):1.0e20);
-	
-    
-    cj = (1.0-beta)*cc + beta*cd;
 
-
-    return cj;
+    return (1.0-beta)*cc + beta*cd;
 }
