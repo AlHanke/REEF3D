6d94c80fd7f358f69878cdab30191474afa96ced
Alexander Hanke <alexander.hanke@ntnu.no>
2026-04-23 20:32:30 +0200

    Made flux limiters inlinable
    Using a common caller inside fluxlim, the implementations only receive doubles to compute the limit using their implementation.

--- src/fluxlim.h
+++ src/fluxlim.h
@@ -23,21 +23,38 @@ Author: Hans Bihs
 #ifndef FLUXLIM_H_
 #define FLUXLIM_H_
 
-class field;
-class fdm;
+#include"increment.h"
+
 class lexer;
 
-using namespace std;
-
-class fluxlim 
+class fluxlim
 {
 public:
     virtual ~fluxlim() = default;
 
-	virtual double iphi(field&,int,int,int,int)=0;
-	virtual double jphi(field&,int,int,int,int)=0;
-	virtual double kphi(field&,int,int,int,int)=0;
+    template<typename GernericField>
+    inline double iphi(const GernericField& b, int n1, int n2, int q1, int q2)
+    {
+        const int i=increment::i, j=increment::j, k=increment::k;
+        return phi_impl(b(i+n1,j,k), b(i+n2,j,k), b(i+q1,j,k), b(i+q2,j,k), b(i,j,k));
+    }
+
+    template<typename GernericField>
+    inline double jphi(const GernericField& b, int n1, int n2, int q1, int q2)
+    {
+        const int i=increment::i, j=increment::j, k=increment::k;
+        return phi_impl(b(i,j+n1,k), b(i,j+n2,k), b(i,j+q1,k), b(i,j+q2,k), b(i,j,k));
+    }
+
+    template<typename GernericField>
+    inline double kphi(const GernericField& b, int n1, int n2, int q1, int q2)
+    {
+        const int i=increment::i, j=increment::j, k=increment::k;
+        return phi_impl(b(i,j,k+n1), b(i,j,k+n2), b(i,j,k+q1), b(i,j,k+q2), b(i,j,k));
+    }
+
+protected:
+    virtual inline double phi_impl(double vn1, double vn2, double vq1, double vq2, double vcell) = 0;
 };
 
 #endif
-
