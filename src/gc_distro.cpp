913c19ca9bafc44690205ab1d67c4f1091b2b65b
Alexander Hanke <alexander.hanke@ntnu.no>
2026-04-27 14:36:04 +0200

    Replaced DIRICHLET BCs with noslip Direct forcing handles the boundaries

--- src/gc_distro.cpp
+++ src/gc_distro.cpp
@@ -31,13 +31,13 @@ void ghostcell::gcdistro(field& f, int ii, int jj, int kk, double dist, bc_label
 
     switch(bc_label)
     {
-        case bc_labels::DIRICHLET_ORTH:
-            dirichlet_ortho(f,dist,cs);
-            break;
         case bc_labels::NEUMANN:
             neumann(f,cs);
             break;
         case bc_labels::NOSLIP:
+        case bc_labels::DIRICHLET_ORTH:
+        case bc_labels::DIRICHLET_ORTH_REFLECT:
+        case bc_labels::DIRICHLET_PARA_REFLECT:
             noslip(f,cs);
             break;
         case bc_labels::OUTFLOWBC:
@@ -45,12 +45,6 @@ void ghostcell::gcdistro(field& f, int ii, int jj, int kk, double dist, bc_label
             break;
         case bc_labels::POTENTIAL:
             potentialbc(f,cs);
-            break;
-        case bc_labels::DIRICHLET_ORTH_REFLECT:
-            dirichlet_ortho_reflect(f,dist,cs);
-            break;
-        case bc_labels::DIRICHLET_PARA_REFLECT:
-            dirichlet_para_reflect(f,dist,cs);
             break;
         case bc_labels::HEATBC:
             heatbc(f,cs);
