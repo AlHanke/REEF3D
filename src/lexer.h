e72df898430e0072452101af5932275dd8204f31
Alexander Hanke <alexander.hanke@ntnu.no>
2026-04-29 17:21:12 +0200

    Moved amrex ini to its own class grid_amrex lexer inherits grid through grid_amrex.

--- src/lexer.h
+++ src/lexer.h
@@ -34,30 +34,26 @@ Author: Hans Bihs
 #include<fstream>
 #include"looping.h"
 #include<vector>
-#include"grid.h"
 #include"control.h"
 #include"coordinates.h"
-
-// AMReX includes
-#include <AMReX_BCRec.H>
-#include <AMReX_MFIter.H>
-#include <AMReX_MultiFab.H>
-#include <AMReX_iMultiFab.H>
-#include <AMReX_Vector.H>
-
-namespace amrex {
-    class Geometry;
-    class BoxArray;
-    class DistributionMapping;
-}
+#if USE_AMREX
+    #include "grid_amrex.h"
+#else
+    #include "grid.h"
+#endif
 
 class weno_nug_func;
 class ghostcell;
 
 using namespace std;
 
-class lexer : virtual public resize_class, public grid, public control,
-              public position, public interpolation, public coordinates
+class lexer : virtual public resize_class, public control,
+              public position, public interpolation, public coordinates,
+#if USE_AMREX
+    public grid_amrex
+#else
+    public grid
+#endif
 {
 public:
 
@@ -87,25 +83,6 @@ public:
 
 
 //-----data-----------------------
-
-    // AMReX Geometry
-    std::vector<amrex::Geometry> amrex_geometry;
-    std::vector<amrex::BoxArray> amrex_box_array;
-    std::vector<amrex::DistributionMapping> amrex_distribution_mapping;
-    std::vector<amrex::Vector<amrex::BCRec>> amrex_bc;
-    const int n_comp = 1;
-    std::vector<amrex::MultiFab> amr_mf;
-    std::unique_ptr<amrex::MFIter> default_mfi;
-    amrex::MFIter* amr_mfi = nullptr;
-    std::vector<amrex::iMultiFab> flag1_imf;
-    std::vector<amrex::iMultiFab> flag2_imf;
-    std::vector<amrex::iMultiFab> flag3_imf;
-    std::vector<amrex::iMultiFab> flag4_imf;
-    std::vector<amrex::iMultiFab> flag7_imf;
-
-    int level;
-    const int nlevs = 1;
-
 	//REEF3D
 
 	int pointnum,cellnum,tpcellnum;
@@ -319,9 +296,6 @@ public:
     double *sig;
     double *sigx,*sigy,*sigz,*sigt;
     double *sigxx;
-
-private:
-    void setup_amrex_geometry(ghostcell*);
 };
 
 #endif
