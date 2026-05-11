21fb7a696e92faacd5516f69924168b8b9fd1897
Alexander Hanke <alexander.hanke@ntnu.no>
2026-05-11 11:07:55 +0200

    !WIP Printer

--- src/printer_CFD.cpp
+++ src/printer_CFD.cpp
@@ -61,6 +61,9 @@ Authors: Hans Bihs, Alexander Hanke
 #include<cstring>
 #if USE_AMREX
     #include <AMReX_PlotFileUtil.H>
+    #include <AMReX_ParallelDescriptor.H>
+    #include <AMReX_FileSystem.H>
+    #include <fstream>
 #endif
 
 printer_CFD::printer_CFD(lexer* p, fdm *a, ghostcell *pgc)
@@ -1063,15 +1066,33 @@ void printer_CFD::print3D(lexer* p, fdm* a, ghostcell* pgc, turbulence *pturb, h
                 plot_mfs_data.push_back(std::move(combined_mf));
             }
 
-            // 3. Generate filename
+            // 3. Generate AMReX plotfile name and metadata
             std::string plotfilename = amrex::Concatenate("REEF3D_CFD_PLT/plt", num, 7);
             amrex::Vector<int> level_steps(p->nlevs, p->count); // Assuming no subcycling for simplicity; adjust if you have subcycling
             amrex::Vector<amrex::IntVect> ref_ratio(p->nlevs-1, p->ref_vec);
+            amrex::Vector<amrex::Geometry> geoms(p->nlevs);
+            for (int lev = 0; lev < p->nlevs; lev++)
+            {
+                auto real_box = p->amrex_geometry[lev].ProbDomain();
+                auto lowEnd = real_box.lo();
+                auto highEnd = real_box.hi();
+                amrex::RealBox real_box_out;
+                real_box_out.setLo(0, p->coordinates::Xout(lowEnd[0], lowEnd[1]));
+                real_box_out.setLo(1, p->coordinates::Yout(lowEnd[0], lowEnd[1]));
+                real_box_out.setLo(2, lowEnd[2]);
+                real_box_out.setHi(0, p->coordinates::Xout(highEnd[0], highEnd[1]));
+                real_box_out.setHi(1, p->coordinates::Yout(highEnd[0], highEnd[1]));
+                real_box_out.setHi(2, highEnd[2]);
+                geoms[lev] = amrex::Geometry(p->amrex_geometry[lev].Domain(), real_box_out, amrex::CoordSys::CoordType::cartesian, p->amrex_geometry[lev].isPeriodic());;
+            }
 
             // 4. Use AMReX utility to write the hierarchical data
             amrex::WriteMultiLevelPlotfile(plotfilename, p->nlevs, GetVecOfConstPtrs(plot_mfs_data), varnames,
-                                        p->amrex_geometry, p->simtime,
+                                        geoms, p->simtime,
                                         level_steps, ref_ratio);
+
+            if (!p->j_dir)
+                print2D_amrex(p, plot_mfs_data, varnames, num);
         }
         #endif
 
@@ -1084,3 +1105,173 @@ void printer_CFD::print3D(lexer* p, fdm* a, ghostcell* pgc, turbulence *pturb, h
         pgc->start4(p,a->topo,150);
     }
 }
+
+#if USE_AMREX
+void printer_CFD::print2D_amrex(lexer* p,
+                                  const amrex::Vector<amrex::MultiFab>& plot_mfs,
+                                  const amrex::Vector<std::string>& varnames,
+                                  int num)
+{
+    struct Cell2D
+    {
+        double x0, z0, dx, dz;
+        std::vector<double> vals;
+    };
+    std::vector<Cell2D> cells;
+    const int nvar = (int)varnames.size();
+
+    // Collect uncovered j=0 cells, finest level first so fine data shadows coarse
+    for (int lev = p->nlevs - 1; lev >= 0; lev--)
+    {
+        const amrex::Geometry& geom = p->amrex_geometry[lev];
+        const double* prob_lo = geom.ProbLo();
+        const double* cell_dx = geom.CellSize();
+
+        for (amrex::MFIter mfi(p->amr_cell_mf[lev]); mfi.isValid(); ++mfi)
+        {
+            const amrex::Box& bx = mfi.validbox();
+            auto const& mask     = p->amr_cell_mf[lev].const_array(mfi);
+            auto const& data     = plot_mfs[lev].const_array(mfi);
+
+            amrex::Loop(bx, [&](int i, int j, int k)
+            {
+                if (j != 0 || mask(i, j, k) != 0) return;
+                Cell2D c;
+                c.x0 = prob_lo[0] + i * cell_dx[0];
+                c.z0 = prob_lo[2] + k * cell_dx[2];
+                c.dx = cell_dx[0];
+                c.dz = cell_dx[2];
+                c.vals.resize(nvar);
+                for (int n = 0; n < nvar; n++)
+                    c.vals[n] = data(i, j, k, n);
+                cells.push_back(c);
+            });
+        }
+    }
+
+    if (p->mpirank == 0)
+        amrex::UtilCreateDirectory("REEF3D_CFD_PLT2D", 0755);
+    amrex::ParallelDescriptor::Barrier();
+
+    const int ncells = (int)cells.size();
+
+    char vtu_name[300];
+    snprintf(vtu_name, sizeof(vtu_name), "REEF3D_CFD_PLT2D/plt%07d_%04d.vtu", num, p->mpirank);
+
+    std::ofstream vtu(vtu_name);
+    vtu << std::scientific;
+    vtu << "<?xml version=\"1.0\"?>\n"
+           "<VTKFile type=\"UnstructuredGrid\" version=\"0.1\" byte_order=\"LittleEndian\">\n"
+           "  <UnstructuredGrid>\n"
+           "    <FieldData>\n"
+           "      <DataArray type=\"Float64\" Name=\"TimeValue\" NumberOfTuples=\"1\" format=\"ascii\">\n"
+           "        " << p->simtime << "\n"
+           "      </DataArray>\n"
+           "    </FieldData>\n"
+           "    <Piece NumberOfPoints=\"" << ncells * 4 << "\" NumberOfCells=\"" << ncells << "\">\n";
+
+    vtu << "      <Points>\n"
+           "        <DataArray type=\"Float64\" NumberOfComponents=\"3\" format=\"ascii\">\n";
+    for (const auto& c : cells)
+    {
+        double x1 = c.x0 + c.dx, z1 = c.z0 + c.dz;
+        vtu << "          " << c.x0 << " 0 " << c.z0 << "\n"
+            << "          " << x1   << " 0 " << c.z0 << "\n"
+            << "          " << x1   << " 0 " << z1   << "\n"
+            << "          " << c.x0 << " 0 " << z1   << "\n";
+    }
+    vtu << "        </DataArray>\n"
+           "      </Points>\n";
+
+    vtu << "      <Cells>\n"
+           "        <DataArray type=\"Int32\" Name=\"connectivity\" format=\"ascii\">\n          ";
+    for (int ci = 0; ci < ncells; ci++)
+        vtu << ci*4 << " " << ci*4+1 << " " << ci*4+2 << " " << ci*4+3 << "  ";
+    vtu << "\n        </DataArray>\n"
+           "        <DataArray type=\"Int32\" Name=\"offsets\" format=\"ascii\">\n          ";
+    for (int ci = 0; ci < ncells; ci++)
+        vtu << (ci + 1) * 4 << " ";
+    vtu << "\n        </DataArray>\n"
+           "        <DataArray type=\"UInt8\" Name=\"types\" format=\"ascii\">\n          ";
+    for (int ci = 0; ci < ncells; ci++)
+        vtu << "9 ";  // VTK_QUAD
+    vtu << "\n        </DataArray>\n"
+           "      </Cells>\n";
+
+    vtu << "      <CellData>\n";
+    for (int n = 0; n < nvar; n++)
+    {
+        vtu << "        <DataArray type=\"Float64\" Name=\"" << varnames[n] << "\" format=\"ascii\">\n          ";
+        for (const auto& c : cells)
+            vtu << c.vals[n] << " ";
+        vtu << "\n        </DataArray>\n";
+    }
+    vtu << "      </CellData>\n"
+           "    </Piece>\n"
+           "  </UnstructuredGrid>\n"
+           "</VTKFile>\n";
+    vtu.close();
+
+    // Rank 0 writes the parallel index file
+    if (p->mpirank == 0)
+    {
+        const int nranks = amrex::ParallelDescriptor::NProcs();
+        char pvtu_name[300];
+        snprintf(pvtu_name, sizeof(pvtu_name), "REEF3D_CFD_PLT2D/plt%07d.pvtu", num);
+        std::ofstream pvtu(pvtu_name);
+        pvtu << "<?xml version=\"1.0\"?>\n"
+                "<VTKFile type=\"PUnstructuredGrid\" version=\"0.1\" byte_order=\"LittleEndian\">\n"
+                "  <PUnstructuredGrid GhostLevel=\"0\">\n"
+                "    <PFieldData>\n"
+                "      <PDataArray type=\"Float64\" Name=\"TimeValue\" NumberOfTuples=\"1\"/>\n"
+                "    </PFieldData>\n"
+                "    <PPoints>\n"
+                "      <PDataArray type=\"Float64\" NumberOfComponents=\"3\"/>\n"
+                "    </PPoints>\n"
+                "    <PCellData>\n";
+        for (const auto& vname : varnames)
+            pvtu << "      <PDataArray type=\"Float64\" Name=\"" << vname << "\"/>\n";
+        pvtu << "    </PCellData>\n";
+        for (int r = 0; r < nranks; r++)
+        {
+            char piece[200];
+            snprintf(piece, sizeof(piece), "plt%07d_%04d.vtu", num, r);
+            pvtu << "    <Piece Source=\"" << piece << "\"/>\n";
+        }
+        pvtu << "  </PUnstructuredGrid>\n"
+                "</VTKFile>\n";
+        pvtu.close();
+
+        // Maintain a PVD collection file so ParaView maps each pvtu to its simulation time
+        constexpr const char pvd_footer[] = "  </Collection>\n</VTKFile>\n";
+        constexpr std::streamoff pvd_footer_len = sizeof(pvd_footer) - 1;
+
+        char pvd_path[300];
+        snprintf(pvd_path, sizeof(pvd_path), "REEF3D_CFD_PLT2D/plt2D.pvd");
+
+        char pvtu_rel[200];
+        snprintf(pvtu_rel, sizeof(pvtu_rel), "plt%07d.pvtu", num);
+
+        char new_entry[300];
+        snprintf(new_entry, sizeof(new_entry),
+                 "    <DataSet timestep=\"%.17g\" part=\"0\" file=\"%s\"/>\n",
+                 p->simtime, pvtu_rel);
+
+        std::fstream pvd;
+        if (std::ifstream(pvd_path).good())
+        {
+            pvd.open(pvd_path, std::ios::in | std::ios::out | std::ios::binary);
+            pvd.seekp(-pvd_footer_len, std::ios::end);
+        }
+        else
+        {
+            pvd.open(pvd_path, std::ios::out | std::ios::binary);
+            pvd << "<?xml version=\"1.0\"?>\n"
+                   "<VTKFile type=\"Collection\" version=\"0.1\" byte_order=\"LittleEndian\">\n"
+                   "  <Collection>\n";
+        }
+        pvd << new_entry << pvd_footer;
+        pvd.close();
+    }
+}
+#endif
