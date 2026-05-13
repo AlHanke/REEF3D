b892447fe72ccf36bd37fb8a2646ec9214158a5c
Alexander Hanke <alexander.hanke@ntnu.no>
2026-05-13 16:57:45 +0200

    First attempt at hypre SSAMG solver integration

---
+++ src/hypre_ssamg.h
@@ -0,0 +1,91 @@
+/*--------------------------------------------------------------------
+REEF3D
+Copyright 2008-2026 Hans Bihs
+
+This file is part of REEF3D.
+
+REEF3D is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------
+Author: Hans Bihs
+--------------------------------------------------------------------*/
+
+#ifndef HYPRE_SSAMG_H_
+#define HYPRE_SSAMG_H_
+
+#include"solver.h"
+#include"increment.h"
+#include"vec.h"
+#include"_hypre_utilities.h"
+#include"HYPRE_sstruct_ls.h"
+#include"HYPRE_parcsr_ls.h"
+#include"HYPRE_krylov.h"
+#include"HYPRE.h"
+
+using namespace std;
+
+class hypre_ssamg final : public solver, public increment
+{
+public:
+
+    hypre_ssamg(lexer*, fdm*, ghostcell*);
+    virtual ~hypre_ssamg();
+
+    void start(lexer*, fdm*, ghostcell*, field&, vec&, int) override final;
+    void startf(lexer*, ghostcell*, field&, vec&, matrix_diag&, int) override final {};
+    void startF(lexer*, ghostcell*, double*, vec&, matrix_diag&, int) override final {};
+    void startV(lexer*, ghostcell*, double*, vec&, matrix_diag&, int) override final {};
+    void startM(lexer*, ghostcell*, double*, double*, double*, int) override final {};
+
+    void start_solver5(lexer*, fdm*, ghostcell*, field&, vec&, int);
+
+    void solve(lexer*);
+
+    void fill_matrix4(lexer*, fdm*, ghostcell*, field&);
+    void fillbackvec4(lexer*, field&, int);
+
+    void make_grid_7p(lexer*, fdm*, ghostcell*);
+
+    void create_solver(lexer*, ghostcell*);
+    void delete_solver(lexer*, ghostcell*);
+
+private:
+
+    HYPRE_SStructGrid     grid;
+    HYPRE_SStructGraph    graph;
+    HYPRE_SStructStencil  stencil;
+    HYPRE_SStructMatrix   A;
+    HYPRE_SStructVector   b;
+    HYPRE_SStructVector   x;
+    HYPRE_SStructSolver   pcg_solver;
+    HYPRE_SStructSolver   ssamg;
+    HYPRE_SStructVariable vartypes[1];
+
+    int numparts;
+    int part;
+    int dimensions;
+    int variable;
+    int numvar;
+    int object_type;
+
+    int *ilower, *iupper;
+    double *values;
+    int num_iterations;
+    double final_res_norm;
+    int stencil_indices[7];
+    int nentries;
+
+    int numiter, count, q;
+};
+
+#endif
