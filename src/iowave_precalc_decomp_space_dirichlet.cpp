97339b686e05da1e1fda384c4d2aba7dcbea8d69
Alexander Hanke <alexander.hanke@ntnu.no>
2026-07-15 18:05:46 +0200

    iowave formatting

--- src/iowave_precalc_decomp_space_dirichlet.cpp
+++ src/iowave_precalc_decomp_space_dirichlet.cpp
@@ -32,15 +32,15 @@ void iowave::wavegen_precalc_space_dirichlet(lexer *p, ghostcell *pgc)
     count=0;
     for(n=0;n<p->gcslin_count;n++)
     {
-    i=p->gcslin[n][0];
-    j=p->gcslin[n][1];
-    
+        i=p->gcslin[n][0];
+        j=p->gcslin[n][1];
+
         xg = xgen(p);
         yg = ygen(p);
-		dg = distgen(p);
-		db = distbeach(p);
-		
-		// Wave Generation
+        dg = distgen(p);
+        db = distbeach(p);
+
+        // Wave Generation
         if(p->B98==3 && h_switch)
         {
             // Zone 1
@@ -48,50 +48,47 @@ void iowave::wavegen_precalc_space_dirichlet(lexer *p, ghostcell *pgc)
             {
                 for(qn=0;qn<wave_comp;++qn)
                 {
-                etaval_S_sin[count][qn] = wave_eta_space_sin(p,pgc,xg,yg,qn);
-                etaval_S_cos[count][qn] = wave_eta_space_cos(p,pgc,xg,yg,qn);
+                    etaval_S_sin[count][qn] = wave_eta_space_sin(p,pgc,xg,yg,qn);
+                    etaval_S_cos[count][qn] = wave_eta_space_cos(p,pgc,xg,yg,qn);
                 }
-            ++count;
+                ++count;
             }
-		}
+        }
     }
     pgc->gcsl_start4(p,eta,50);
-    
-
 
     count=0;
     for(n=0;n<p->gcslin_count;n++)
     {
-    i=p->gcslin[n][0];
-    j=p->gcslin[n][1];
-    
-    xg = xgen1(p);
-    yg = ygen1(p);
-        
+        i=p->gcslin[n][0];
+        j=p->gcslin[n][1];
+
+        xg = xgen1(p);
+        yg = ygen1(p);
+
         KLOOP
         UCHECK
         {
-        zloc1 = p->pos1_z();
+            zloc1 = p->pos1_z();
 
-        if(zloc1<=p->phimean)
-        z=-(fabs(p->phimean-zloc1));
-		
-		if(zloc1>p->phimean)
-        z=(fabs(p->phimean-zloc1));
-        
-        if(zloc1>p->phimean+p->wA)
-        z = 0.5*(eta(i,j)+eta(i+1,j));
+            if(zloc1<=p->phimean)
+                z=-(fabs(p->phimean-zloc1));
+            else
+                z=(fabs(p->phimean-zloc1));
 
-		// Wave Generation
-		if(p->B98>=3 && u_switch)
-        {
+            if(zloc1>p->phimean+p->wA)
+            z = 0.5*(eta(i,j)+eta(i+1,j));
+
+            // Wave Generation
+            if(p->B98>=3 && u_switch)
+            {
                 for(qn=0;qn<wave_comp;++qn)
                 {
-                uval_S_sin[count][qn] = wave_u_space_sin(p,pgc,xg,yg,z,qn);
-                uval_S_cos[count][qn] = wave_u_space_cos(p,pgc,xg,yg,z,qn);
+                    uval_S_sin[count][qn] = wave_u_space_sin(p,pgc,xg,yg,z,qn);
+                    uval_S_cos[count][qn] = wave_u_space_cos(p,pgc,xg,yg,z,qn);
                 }
-            ++count;
-		}
+                ++count;
+            }
         }
     }
 
@@ -99,117 +96,109 @@ void iowave::wavegen_precalc_space_dirichlet(lexer *p, ghostcell *pgc)
     count=0;
     for(n=0;n<p->gcslin_count;n++)
     {
-    i=p->gcslin[n][0];
-    j=p->gcslin[n][1];
-    
-    xg = xgen2(p);
-    yg = ygen2(p);
-        
+        i=p->gcslin[n][0];
+        j=p->gcslin[n][1];
+
+        xg = xgen2(p);
+        yg = ygen2(p);
+
         KLOOP
         VCHECK
         {
-        zloc2 = p->pos2_z();
+            zloc2 = p->pos2_z();
 
-        if(zloc2<=p->phimean)
-        z=-(fabs(p->phimean-zloc2));
-		
-		if(zloc2>p->phimean)
-        z=(fabs(p->phimean-zloc2));
-        
-        if(zloc2>p->phimean+p->wA)
-        z = 0.5*(eta(i,j)+eta(i,j+1));
-        
-		// Wave Generation		
-		if(p->B98>=3 && v_switch)
-        {
+            if(zloc2<=p->phimean)
+                z=-(fabs(p->phimean-zloc2));
+            else
+                z=(fabs(p->phimean-zloc2));
+
+            if(zloc2>p->phimean+p->wA)
+            z = 0.5*(eta(i,j)+eta(i,j+1));
+
+            // Wave Generation
+            if(p->B98>=3 && v_switch)
+            {
                 for(qn=0;qn<wave_comp;++qn)
                 {
-                vval_S_sin[count][qn] = wave_v_space_sin(p,pgc,xg,yg,z,qn);
-                vval_S_cos[count][qn] = wave_v_space_cos(p,pgc,xg,yg,z,qn);
+                    vval_S_sin[count][qn] = wave_v_space_sin(p,pgc,xg,yg,z,qn);
+                    vval_S_cos[count][qn] = wave_v_space_cos(p,pgc,xg,yg,z,qn);
                 }
-            ++count;
-		}
+                ++count;
+            }
         }
     }
 
-
-    
-    count=0;
     count=0;
     for(n=0;n<p->gcslin_count;n++)
     {
-    i=p->gcslin[n][0];
-    j=p->gcslin[n][1];
-    
-    xg = xgen(p);
-    yg = ygen(p);
-        
+        i=p->gcslin[n][0];
+        j=p->gcslin[n][1];
+
+        xg = xgen(p);
+        yg = ygen(p);
+
         KWLOOP
         WCHECK
         {
-        zloc3 = p->pos3_z();
+            zloc3 = p->pos3_z();
 
-        if(zloc3<=p->phimean)
-        z=-(fabs(p->phimean-zloc3));
-		
-		if(zloc3>p->phimean)
-        z=(fabs(p->phimean-zloc3));
-        
-        if(zloc3>p->phimean+p->wA)
-        z = eta(i,j);
-        
-		// Wave Generation
-		if(p->B98>=3 && w_switch)
-        {
+            if(zloc3<=p->phimean)
+                z=-(fabs(p->phimean-zloc3));
+            else
+                z=(fabs(p->phimean-zloc3));
+
+            if(zloc3>p->phimean+p->wA)
+            z = eta(i,j);
+
+            // Wave Generation
+            if(p->B98>=3 && w_switch)
+            {
                 for(qn=0;qn<wave_comp;++qn)
                 {
-                wval_S_sin[count][qn] = wave_w_space_sin(p,pgc,xg,yg,z,qn);
-                wval_S_cos[count][qn] = wave_w_space_cos(p,pgc,xg,yg,z,qn);
+                    wval_S_sin[count][qn] = wave_w_space_sin(p,pgc,xg,yg,z,qn);
+                    wval_S_cos[count][qn] = wave_w_space_cos(p,pgc,xg,yg,z,qn);
                 }
-            ++count;
-		}
+                ++count;
+            }
         }
-    }	
+    }
 
-
-    
     count=0;
     if(f_switch)
     for(n=0;n<p->gcslin_count;n++)
     {
-    i=p->gcslin[n][0];
-    j=p->gcslin[n][1];
-    
-    xg = xgen1(p);
-    yg = ygen1(p);
-    dg = distgen(p);
-    db = distbeach(p);
-        
+        i=p->gcslin[n][0];
+        j=p->gcslin[n][1];
+
+        xg = xgen1(p);
+        yg = ygen1(p);
+        dg = distgen(p);
+        db = distbeach(p);
+
         KLOOP
         PCHECK
         {
-        zloc4 = p->pos_z();
+            zloc4 = p->pos_z();
 
-        if(zloc4<=p->phimean)
-        z=-(fabs(p->phimean-zloc4));
-		
-		if(zloc4>p->phimean)
-        z=(fabs(p->phimean-zloc4));
-		
-		// Wave Generation
-		if(p->B98==3)
-        {
-            // Zone 1
-            if(dg<dist1)
+            if(zloc4<=p->phimean)
+                z=-(fabs(p->phimean-zloc4));
+            else
+                z=(fabs(p->phimean-zloc4));
+
+            // Wave Generation
+            if(p->B98==3)
             {
-                for(qn=0;qn<wave_comp;++qn)
+                // Zone 1
+                if(dg<dist1)
                 {
-                Fival_S_sin[count][qn] = wave_fi_space_sin(p,pgc,xg,yg,z,qn);
-                Fival_S_cos[count][qn] = wave_fi_space_cos(p,pgc,xg,yg,z,qn);
+                    for(qn=0;qn<wave_comp;++qn)
+                    {
+                        Fival_S_sin[count][qn] = wave_fi_space_sin(p,pgc,xg,yg,z,qn);
+                        Fival_S_cos[count][qn] = wave_fi_space_cos(p,pgc,xg,yg,z,qn);
+                    }
+                    ++count;
                 }
-            ++count;
             }
-		}
         }
     }
 
