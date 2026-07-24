ed32f98afb474738bd8d9f0203c6a8eb8dc63e2d
Alexander Hanke <alexander.hanke@ntnu.no>
2026-07-24 14:10:23 +0200

    !WIP

--- src/solver.h
+++ src/solver.h
@@ -43,7 +43,7 @@ class solver
 public:
     virtual ~solver() = default;
 
-	virtual void start(lexer*,fdm*, ghostcell*, field&, vec&, int)=0;
+	virtual void start(lexer*, fdm*, ghostcell*, field&, vec&, int)=0;
     virtual void startf(lexer*, ghostcell*, field&, vec&, matrix_diag&, int)=0;
     virtual void startF(lexer*, ghostcell*, double*, vec&, matrix_diag&, int)=0;
     virtual void startV(lexer*, ghostcell*, double*, vec&, matrix_diag&, int)=0;
