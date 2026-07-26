da2cb89488051417e2232f574d4392002e87b273
Alexander Hanke <alexander.hanke@ntnu.no>
2026-07-26 17:15:38 +0200

    Formatted iowave_*_precalc_ini

--- src/iowave_nhflow_precalc_ini.cpp
+++ src/iowave_nhflow_precalc_ini.cpp
@@ -80,29 +80,27 @@ void iowave::nhflow_precalc_relax_ini(lexer *p,fdm_nhf *d, ghostcell *pgc)
     p->Darray(VHval,upt_count);
     p->Darray(WHval,upt_count);
 
-    
-    
     if(p->B89==1) 
     {
-    p->Farray(uval_S_sin,upt_count,wave_comp);
-    p->Farray(vval_S_sin,upt_count,wave_comp);
-    p->Farray(wval_S_sin,upt_count,wave_comp);
-    p->Darray(etaval_S_sin,ept_count,wave_comp);
+        p->Farray(uval_S_sin,upt_count,wave_comp);
+        p->Farray(vval_S_sin,upt_count,wave_comp);
+        p->Farray(wval_S_sin,upt_count,wave_comp);
+        p->Darray(etaval_S_sin,ept_count,wave_comp);
 
-    p->Farray(uval_S_cos,upt_count,wave_comp);
-    p->Farray(vval_S_cos,upt_count,wave_comp);
-    p->Farray(wval_S_cos,upt_count,wave_comp);
-    p->Darray(etaval_S_cos,ept_count,wave_comp);
+        p->Farray(uval_S_cos,upt_count,wave_comp);
+        p->Farray(vval_S_cos,upt_count,wave_comp);
+        p->Farray(wval_S_cos,upt_count,wave_comp);
+        p->Darray(etaval_S_cos,ept_count,wave_comp);
 
-    p->Darray(uval_T_sin,wave_comp);
-    p->Darray(vval_T_sin,wave_comp);
-    p->Darray(wval_T_sin,wave_comp);
-    p->Darray(etaval_T_sin,wave_comp);
-    
-    p->Darray(uval_T_cos,wave_comp);
-    p->Darray(vval_T_cos,wave_comp);
-    p->Darray(wval_T_cos,wave_comp);
-    p->Darray(etaval_T_cos,wave_comp);
+        p->Darray(uval_T_sin,wave_comp);
+        p->Darray(vval_T_sin,wave_comp);
+        p->Darray(wval_T_sin,wave_comp);
+        p->Darray(etaval_T_sin,wave_comp);
+
+        p->Darray(uval_T_cos,wave_comp);
+        p->Darray(vval_T_cos,wave_comp);
+        p->Darray(wval_T_cos,wave_comp);
+        p->Darray(etaval_T_cos,wave_comp);
     }
 }
 
@@ -123,35 +121,33 @@ void iowave::nhflow_precalc_dirichlet_ini(lexer *p, fdm_nhf *d, ghostcell *pgc)
     }
   
     // precalc array alloc
-
     p->Darray(uval,upt_count);
     p->Darray(vval,upt_count);
     p->Darray(wval,upt_count);
     p->Darray(UHval,upt_count);
     p->Darray(VHval,upt_count);
     p->Darray(WHval,upt_count);
-    
+
     if(p->B89==1) 
     {
-    p->Farray(uval_S_sin,upt_count,wave_comp);
-    p->Farray(vval_S_sin,upt_count,wave_comp);
-    p->Farray(wval_S_sin,upt_count,wave_comp);
-    p->Darray(etaval_S_sin,ept_count,wave_comp);
+        p->Farray(uval_S_sin,upt_count,wave_comp);
+        p->Farray(vval_S_sin,upt_count,wave_comp);
+        p->Farray(wval_S_sin,upt_count,wave_comp);
+        p->Darray(etaval_S_sin,ept_count,wave_comp);
 
-    p->Farray(uval_S_cos,upt_count,wave_comp);
-    p->Farray(vval_S_cos,upt_count,wave_comp);
-    p->Farray(wval_S_cos,upt_count,wave_comp);
-    p->Darray(etaval_S_cos,ept_count,wave_comp);
+        p->Farray(uval_S_cos,upt_count,wave_comp);
+        p->Farray(vval_S_cos,upt_count,wave_comp);
+        p->Farray(wval_S_cos,upt_count,wave_comp);
+        p->Darray(etaval_S_cos,ept_count,wave_comp);
 
-    p->Darray(uval_T_sin,wave_comp);
-    p->Darray(vval_T_sin,wave_comp);
-    p->Darray(wval_T_sin,wave_comp);
-    p->Darray(etaval_T_sin,wave_comp);
-    
-    p->Darray(uval_T_cos,wave_comp);
-    p->Darray(vval_T_cos,wave_comp);
-    p->Darray(wval_T_cos,wave_comp);
-    p->Darray(etaval_T_cos,wave_comp);
+        p->Darray(uval_T_sin,wave_comp);
+        p->Darray(vval_T_sin,wave_comp);
+        p->Darray(wval_T_sin,wave_comp);
+        p->Darray(etaval_T_sin,wave_comp);
+
+        p->Darray(uval_T_cos,wave_comp);
+        p->Darray(vval_T_cos,wave_comp);
+        p->Darray(wval_T_cos,wave_comp);
+        p->Darray(etaval_T_cos,wave_comp);
     }
-    
 }
