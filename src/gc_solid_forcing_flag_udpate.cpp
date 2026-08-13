e824fce7bbbc039d8ff1babb174aa17f92d5b84e
Alexander Hanke <alexander.hanke@ntnu.no>
2026-08-13 13:24:14 +0200

    Formatted gc_solid_forcing_flag_update.cpp

--- src/gc_solid_forcing_flag_udpate.cpp
+++ src/gc_solid_forcing_flag_udpate.cpp
@@ -25,57 +25,54 @@ Authors: Hans Bihs
 #include"fdm.h"
 
 void ghostcell::solid_forcing_flag_update(lexer *p, fdm *a)
-{   
+{
     // Update DF
     LOOP
-    p->DF[IJK]=1;
-    
-    if(p->solidread>0)
-    LOOP
-    if(a->solid(i,j,k)<0.0)
-    p->DF[IJK]=-1;
-    
-    
-    if(p->toporead>0)
-    LOOP
-    if(a->topo(i,j,k)<0.0)
-    p->DF[IJK]=-1;
-    
-    if(p->X10>0)
-    LOOP
-    if(a->fb(i,j,k)<0.0)
-    p->DF[IJK]=-1;
-    
+    {
+        p->DF[IJK]=1;
+
+        if((p->solidread>0 && a->solid(i,j,k)<0.0)
+        || (p->toporead>0 && a->topo(i,j,k)<0.0)
+        || (p->X10>0 && a->fb(i,j,k)<0.0))
+        p->DF[IJK]=-1;
+    }
+
     startintV(p,p->DF,1);
-    
     
     // 1
     ULOOP
-    p->DF1[IJK]=p->DF[IJK];
-    
-    ULOOP
-    if(p->DF[IJK]>0 && p->DF[Ip1JK]<0)
-    p->DF1[IJK]=-1;
-    
+    {
+        int df=p->DF[IJK];
+
+        if(df>0 && p->DF[Ip1JK]<0)
+            df=-1;
+
+        p->DF1[IJK]=df;
+    }
+
     // 2
     VLOOP
-    p->DF2[IJK]=p->DF[IJK];
-    
-    VLOOP
-    if(p->DF[IJK]>0 && p->DF[IJp1K]<0)
-    p->DF2[IJK]=-1;
-    
+    {
+        int df=p->DF[IJK];
+
+        if(df>0 && p->DF[IJp1K]<0)
+            df=-1;
+
+        p->DF2[IJK]=df;
+    }
+
     // 3
     WLOOP
-    p->DF3[IJK]=p->DF[IJK];
-    
-    WLOOP
-    if(p->DF[IJK]>0 && p->DF[IJKp1]<0)
-    p->DF3[IJK]=-1;
-    
-    
+    {
+        int df=p->DF[IJK];
+
+        if(df>0 && p->DF[IJKp1]<0)
+            df=-1;
+
+        p->DF3[IJK]=df;
+    }
+
     gcparaxintV(p, p->DF1, 1);
     gcparaxintV(p, p->DF2, 1);
     gcparaxintV(p, p->DF3, 1);
-
 }
