c62f7cdba43db3e402d151d5b80fd839cafdbcb0
Alexander Hanke <alexander.hanke@ntnu.no>
2026-08-13 13:08:39 +0200

    Replace gcdf* with std::vector<std::array>>

--- src/lexer.h
+++ src/lexer.h
@@ -104,7 +104,8 @@ public:
 	int **gcb1,**gcb2,**gcb3,**gcb4;
 
     int gcdf1_count,gcdf2_count,gcdf3_count,gcdf4_count;
-    int **gcdf1,**gcdf2,**gcdf3,**gcdf4;
+    std::vector<std::array<int, 5>> gcdf1, gcdf2, gcdf3;
+    std::vector<std::array<int, 6>> gcdf4;
     int gcsldfeta4_count,gcsldfbed4_count;
     int **gcsldfeta4,**gcsldfbed4;
 
