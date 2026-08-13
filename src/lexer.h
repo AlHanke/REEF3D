799c1f8a9ff2609f732ee118d2f007f1ef7fed81
Alexander Hanke <alexander.hanke@ntnu.no>
2026-08-13 13:02:49 +0200

    Replace flagsf1-3 with lambdas

--- src/lexer.h
+++ src/lexer.h
@@ -85,7 +85,7 @@ public:
     int N7,N7_row,N7_col;
 	int surf_tot;
 	int *flag1,*flag2,*flag3,*flag4,*flag5,*flag7;
-    int *flagsf1,*flagsf2,*flagsf3,*flagsf4;
+    int *flagsf4;
 
     // flag
     double *flag_solid,*flag_topo;
