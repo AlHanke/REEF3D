95335a2a40b9ba46a57081edf1026799189644a4
Alexander Hanke <alexander.hanke@ntnu.no>
2026-08-19 09:27:35 +0200

    Removed ghostcell::dgcslpol

--- src/printer_nhflow.cpp
+++ src/printer_nhflow.cpp
@@ -383,10 +383,6 @@ void printer_nhflow::print(lexer* p, fdm_nhf *d, ghostcell* pgc, nhflow_turbulen
         pgc->gcsl_start4(p,d->breaking_print,50);
         pgc->start5V(p,d->test,1);
 
-        pgc->dgcslpol(p,d->WL,p->dgcsl4,p->dgcsl4_count,14);
-        pgc->dgcslpol(p,d->breaking_print,p->dgcsl4,p->dgcsl4_count,14);
-        pgc->dgcslpol(p,d->bed,p->dgcsl4,p->dgcsl4_count,14);
-
         i=-1;
         j=-1;
         if(i+p->origin_i==-1 && j+p->origin_j==-1 )
