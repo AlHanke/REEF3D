284a1b000d80cc851446bf3e4aba6764e6684714
Alexander Hanke <alexander.hanke@ntnu.no>
2026-09-10 14:52:19 +0200

    Added 2D guard to ediff2

--- src/ediff2.cpp
+++ src/ediff2.cpp
@@ -31,13 +31,15 @@ ediff2::ediff2(lexer *p) : gradient(p)
 
 void ediff2::diff_scalar(lexer *p, fdm *a, ghostcell *pgc, solver *psolv, field &b, field &visc, field &eddyv, double sig, double alpha)
 {
+    const bool is3D = p->j_dir;
+
     LOOP
     {
         a->L(i,j,k) += ((b(i+1,j,k)-b(i,j,k))*0.5*(visc(i+1,j,k)+eddyv(i+1,j,k)/sig+visc(i,j,k)+eddyv(i,j,k)/sig)*(1.0/p->DXP[IP])
             -(b(i,j,k)-b(i-1,j,k))*0.5*(visc(i,j,k)+eddyv(i,j,k)/sig+visc(i-1,j,k)+eddyv(i-1,j,k)/sig)*(1.0/p->DXP[IM1]))/p->DXN[IP]
 
-            +((b(i,j+1,k)-b(i,j,k))*0.5*(visc(i,j+1,k)+eddyv(i,j+1,k)/sig+visc(i,j,k)+eddyv(i,j,k)/sig)*(1.0/p->DYP[JP])
-            -(b(i,j,k)-b(i,j-1,k))*0.5*(visc(i,j,k)+eddyv(i,j,k)/sig+visc(i,j-1,k)+eddyv(i,j-1,k)/sig)*(1.0/p->DYP[JM1]))/p->DYN[JP]
+            +(is3D ? ((b(i,j+1,k)-b(i,j,k))*0.5*(visc(i,j+1,k)+eddyv(i,j+1,k)/sig+visc(i,j,k)+eddyv(i,j,k)/sig)*(1.0/p->DYP[JP])
+            -(b(i,j,k)-b(i,j-1,k))*0.5*(visc(i,j,k)+eddyv(i,j,k)/sig+visc(i,j-1,k)+eddyv(i,j-1,k)/sig)*(1.0/p->DYP[JM1]))/p->DYN[JP] : 0.0)
 
             +((b(i,j,k+1)-b(i,j,k))*0.5*(visc(i,j,k+1)+eddyv(i,j,k+1)/sig+visc(i,j,k)+eddyv(i,j,k)/sig)*(1.0/p->DZP[KP])
             -(b(i,j,k)-b(i,j,k-1))*0.5*(visc(i,j,k)+eddyv(i,j,k)/sig+visc(i,j,k-1)+eddyv(i,j,k-1)/sig)*(1.0/p->DZP[KM1]))/p->DZN[KP];
@@ -46,13 +48,15 @@ void ediff2::diff_scalar(lexer *p, fdm *a, ghostcell *pgc, solver *psolv, field 
 
 void ediff2::diff_scalar(lexer *p, fdm *a, ghostcell *pgc, solver *psolv, field &diff, field &b, field &visc, field &eddyv, double sig, double alpha)
 {
+    const bool is3D = p->j_dir;
+
     LOOP
     {
         a->L(i,j,k) += ((b(i+1,j,k)-b(i,j,k))*0.5*(visc(i+1,j,k)+eddyv(i+1,j,k)/sig+visc(i,j,k)+eddyv(i,j,k)/sig)*(1.0/p->DXP[IP])
             -(b(i,j,k)-b(i-1,j,k))*0.5*(visc(i,j,k)+eddyv(i,j,k)/sig+visc(i-1,j,k)+eddyv(i-1,j,k)/sig)*(1.0/p->DXP[IM1]))/p->DXN[IP]
 
-            +((b(i,j+1,k)-b(i,j,k))*0.5*(visc(i,j+1,k)+eddyv(i,j+1,k)/sig+visc(i,j,k)+eddyv(i,j,k)/sig)*(1.0/p->DYP[JP])
-            -(b(i,j,k)-b(i,j-1,k))*0.5*(visc(i,j,k)+eddyv(i,j,k)/sig+visc(i,j-1,k)+eddyv(i,j-1,k)/sig)*(1.0/p->DYP[JM1]))/p->DYN[JP]
+            +(is3D ? ((b(i,j+1,k)-b(i,j,k))*0.5*(visc(i,j+1,k)+eddyv(i,j+1,k)/sig+visc(i,j,k)+eddyv(i,j,k)/sig)*(1.0/p->DYP[JP])
+            -(b(i,j,k)-b(i,j-1,k))*0.5*(visc(i,j,k)+eddyv(i,j,k)/sig+visc(i,j-1,k)+eddyv(i,j-1,k)/sig)*(1.0/p->DYP[JM1]))/p->DYN[JP] : 0.0)
 
             +((b(i,j,k+1)-b(i,j,k))*0.5*(visc(i,j,k+1)+eddyv(i,j,k+1)/sig+visc(i,j,k)+eddyv(i,j,k)/sig)*(1.0/p->DZP[KP])
             -(b(i,j,k)-b(i,j,k-1))*0.5*(visc(i,j,k)+eddyv(i,j,k)/sig+visc(i,j,k-1)+eddyv(i,j,k-1)/sig)*(1.0/p->DZP[KM1]))/p->DZN[KP];
@@ -75,32 +79,43 @@ void ediff2::diff_u(lexer *p, fdm *a, ghostcell *pgc, solver *psolv, field &diff
     double u_ijk,ev_ijk,visc_ijk;
     double visc_ddy_p,visc_ddy_m,visc_ddz_p,visc_ddz_m;
 
+    const bool is3D = p->j_dir;
+
     ULOOP
     {
         u_ijk=u(i,j,k);
         ev_ijk=a->eddyv(i,j,k);
         visc_ijk=a->visc(i,j,k);
 
-        visc_ddy_p = (visc_ijk+ev_ijk + a->visc(i+1,j,k)+a->eddyv(i+1,j,k) + a->visc(i,j+1,k)+a->eddyv(i,j+1,k) + a->visc(i+1,j+1,k)+a->eddyv(i+1,j+1,k))*0.25;
-        visc_ddy_m = (a->visc(i,j-1,k)+a->eddyv(i,j-1,k)  +a->visc(i+1,j-1,k)+a->eddyv(i+1,j-1,k) + visc_ijk+ev_ijk + a->visc(i+1,j,k)+a->eddyv(i+1,j,k))*0.25;
+        // Pseudo-2D: the j+-1 samples are the same cell, so the 4-point face average
+        // degenerates to the 2-point average over the surviving directions. Spelling that
+        // out keeps the coefficient right even if the y-index fold is ever removed.
+        visc_ddy_p = is3D ? (visc_ijk+ev_ijk + a->visc(i+1,j,k)+a->eddyv(i+1,j,k) + a->visc(i,j+1,k)+a->eddyv(i,j+1,k) + a->visc(i+1,j+1,k)+a->eddyv(i+1,j+1,k))*0.25
+                          : (visc_ijk+ev_ijk + a->visc(i+1,j,k)+a->eddyv(i+1,j,k))*0.5;
+        visc_ddy_m = is3D ? (a->visc(i,j-1,k)+a->eddyv(i,j-1,k)  +a->visc(i+1,j-1,k)+a->eddyv(i+1,j-1,k) + visc_ijk+ev_ijk + a->visc(i+1,j,k)+a->eddyv(i+1,j,k))*0.25
+                          : visc_ddy_p;
         visc_ddz_p = (visc_ijk+ev_ijk + a->visc(i+1,j,k)+a->eddyv(i+1,j,k) + a->visc(i,j,k+1)+a->eddyv(i,j,k+1) + a->visc(i+1,j,k+1)+a->eddyv(i+1,j,k+1))*0.25;
         visc_ddz_m = (a->visc(i,j,k-1)+a->eddyv(i,j,k-1) + a->visc(i+1,j,k-1)+a->eddyv(i+1,j,k-1) + visc_ijk+ev_ijk + a->visc(i+1,j,k)+a->eddyv(i+1,j,k))*0.25;
 
         a->F(i,j,k) +=  2.0*((u(i+1,j,k)-u_ijk)*((a->visc(i+1,j,k)+a->eddyv(i+1,j,k))/p->DXN[IP1])
                             - (u_ijk-u(i-1,j,k))*((visc_ijk+ev_ijk)/p->DXN[IP]))/p->DXP[IP]
-                        + ((u(i,j+1,k)-u_ijk)*(visc_ddy_p/p->DYP[JP])
-                            - (u_ijk-u(i,j-1,k))*(visc_ddy_m/p->DYP[JM1]))/p->DYN[JP]
+                        + (is3D ? ((u(i,j+1,k)-u_ijk)*(visc_ddy_p/p->DYP[JP])
+                            - (u_ijk-u(i,j-1,k))*(visc_ddy_m/p->DYP[JM1]))/p->DYN[JP] : 0.0)
                         + ((u(i,j,k+1)-u_ijk)*(visc_ddz_p/p->DZP[KP])
                             - (u_ijk-u(i,j,k-1))*(visc_ddz_m/p->DZP[KM1]))/p->DZN[KP]
 
-                        + ((v(i+1,j,k)-v(i,j,k))*(visc_ddy_p/p->DXP[IP])
-                            - (v(i+1,j-1,k)-v(i,j-1,k))*(visc_ddy_m/p->DXP[IP]))/p->DYN[JP]
+                        + (is3D ? ((v(i+1,j,k)-v(i,j,k))*(visc_ddy_p/p->DXP[IP])
+                            - (v(i+1,j-1,k)-v(i,j-1,k))*(visc_ddy_m/p->DXP[IP]))/p->DYN[JP] : 0.0)
 
                         + ((w(i+1,j,k)-w(i,j,k))*(visc_ddz_p/p->DXP[IP])
                             - (w(i+1,j,k-1)-w(i,j,k-1))*(visc_ddz_m/p->DXP[IP]))/p->DZN[KP];
     }
 }
 
+// No j_dir guard here: VLOOP is JVLOOP-bounded, which is an empty range at knoy==1
+// (j = 0; j <= JMAX_LOOP-1 == -1), so both loops below are already no-ops in pseudo-2D.
+// An early return would additionally skip the pgc->start2 below, which is a behaviour
+// change rather than a guard.
 void ediff2::diff_v(lexer *p, fdm *a, ghostcell *pgc, solver *psolv, field &diff, field &v_in, field &u, field &v, field &w, double alpha)
 {
     VLOOP
@@ -146,6 +161,8 @@ void ediff2::diff_w(lexer *p, fdm *a, ghostcell *pgc, solver *psolv, field &diff
     double w_ijk,ev_ijk,visc_ijk;
     double visc_ddx_p,visc_ddx_m,visc_ddy_p,visc_ddy_m;
 
+    const bool is3D = p->j_dir;
+
     WLOOP
     {
         w_ijk=w(i,j,k);
@@ -153,21 +170,24 @@ void ediff2::diff_w(lexer *p, fdm *a, ghostcell *pgc, solver *psolv, field &diff
         visc_ijk=a->visc(i,j,k);
         visc_ddx_p = (visc_ijk+ev_ijk + a->visc(i,j,k+1)+a->eddyv(i,j,k+1) + a->visc(i+1,j,k)+a->eddyv(i+1,j,k) + a->visc(i+1,j,k+1)+a->eddyv(i+1,j,k+1))*0.25;
         visc_ddx_m = (a->visc(i-1,j,k)+a->eddyv(i-1,j,k) + a->visc(i-1,j,k+1)+a->eddyv(i-1,j,k+1) + visc_ijk+ev_ijk + a->visc(i,j,k+1)+a->eddyv(i,j,k+1))*0.25;
-        visc_ddy_p = (visc_ijk+ev_ijk + a->visc(i,j,k+1)+a->eddyv(i,j,k+1) + a->visc(i,j+1,k)+a->eddyv(i,j+1,k) + a->visc(i,j+1,k+1)+a->eddyv(i,j+1,k+1))*0.25;
-        visc_ddy_m = (a->visc(i,j-1,k)+a->eddyv(i,j-1,k) + a->visc(i,j-1,k+1)+a->eddyv(i,j-1,k+1) + visc_ijk+ev_ijk + a->visc(i,j,k+1)+a->eddyv(i,j,k+1))*0.25;
+        // Pseudo-2D: see diff_u -- the 4-point average degenerates to the 2-point one.
+        visc_ddy_p = is3D ? (visc_ijk+ev_ijk + a->visc(i,j,k+1)+a->eddyv(i,j,k+1) + a->visc(i,j+1,k)+a->eddyv(i,j+1,k) + a->visc(i,j+1,k+1)+a->eddyv(i,j+1,k+1))*0.25
+                          : (visc_ijk+ev_ijk + a->visc(i,j,k+1)+a->eddyv(i,j,k+1))*0.5;
+        visc_ddy_m = is3D ? (a->visc(i,j-1,k)+a->eddyv(i,j-1,k) + a->visc(i,j-1,k+1)+a->eddyv(i,j-1,k+1) + visc_ijk+ev_ijk + a->visc(i,j,k+1)+a->eddyv(i,j,k+1))*0.25
+                          : visc_ddy_p;
 
         a->H(i,j,k) +=  ((w(i+1,j,k)-w_ijk)*(visc_ddx_p/p->DXP[IP])
                             - (w_ijk-w(i-1,j,k))*(visc_ddx_m/p->DXP[IM1]))/p->DXN[IP]
-                        + ((w(i,j+1,k)-w_ijk)*(visc_ddy_p/p->DYP[JP])
-                            -(w_ijk-w(i,j-1,k))*(visc_ddy_m/p->DYP[JM1]))/p->DYN[JP]
+                        + (is3D ? ((w(i,j+1,k)-w_ijk)*(visc_ddy_p/p->DYP[JP])
+                            -(w_ijk-w(i,j-1,k))*(visc_ddy_m/p->DYP[JM1]))/p->DYN[JP] : 0.0)
                         + 2.0*((w(i,j,k+1)-w_ijk)*((a->visc(i,j,k+1)+a->eddyv(i,j,k+1))/p->DZN[KP1])
                             -(w_ijk-w(i,j,k-1))*((visc_ijk+ev_ijk)/p->DZN[KP]))/p->DZP[KP]
 
                         + ((u(i,j,k+1)-u(i,j,k))*(visc_ddx_p/p->DZP[KP])
                             - (u(i-1,j,k+1)-u(i-1,j,k))*(visc_ddx_m/p->DZP[KP]))/p->DXN[IP]
 
-                        + ((v(i,j,k+1)-v(i,j,k))*(visc_ddy_p/p->DZP[KP])
-                            - (v(i,j-1,k+1)-v(i,j-1,k))*(visc_ddy_m/p->DZP[KP]))/p->DYN[JP];
+                        + (is3D ? ((v(i,j,k+1)-v(i,j,k))*(visc_ddy_p/p->DZP[KP])
+                            - (v(i,j-1,k+1)-v(i,j-1,k))*(visc_ddy_m/p->DZP[KP]))/p->DYN[JP] : 0.0);
     }
 }
 
