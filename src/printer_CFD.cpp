2fdf3c6747ff1621de4b5c2e3ea295bfebbf9154
Alexander Hanke <alexander.hanke@ntnu.no>
2026-09-12 10:31:29 +0200

    !Pre-Printer fix

--- src/printer_CFD.cpp
+++ src/printer_CFD.cpp
@@ -314,13 +314,13 @@ void printer_CFD::start(lexer* p, fdm* a, ghostcell* pgc, turbulence *pturb, hea
     pflowfile->start(p,a,pgc,pturb);
 
     // Print state out based on iteration
-    if(p->count%p->P41==0 && p->P42<0.0 && p->P40>0 && p->P41>0 && (p->P46==0 || (p->count>=p->P46_is && p->count<<p->P46_ie)))
+    if(p->count%p->P41==0 && p->P42<0.0 && p->P40>0 && p->P41>0 && (p->P46==0 || (p->count>=p->P46_is && p->count<p->P46_ie)))
     {
         pstate->write(p,a,pgc,pturb,psed);
     }
 
     // Print state out based on time
-    if((p->simtime>p->stateprinttime && p->P42>0.0 || (p->count==0 &&  p->P42>0.0)) && p->P40>0 && (p->P47==0 || (p->count>=p->P47_ts && p->count<<p->P47_te)))
+    if(((p->simtime>p->stateprinttime || p->count==0) && p->P42>0.0) && p->P40>0 && (p->P47==0 || (p->count>=p->P47_ts && p->count<p->P47_te)))
     {
         pstate->write(p,a,pgc,pturb,psed);
 
