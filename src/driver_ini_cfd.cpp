811c2d85b8f797ff6aae4faf1fe8052daad0b641
Alexander Hanke <alexander.hanke@ntnu.no>
2026-09-15 17:41:22 +0200

    !Merge !WIP - driver_ini

--- src/driver_ini_cfd.cpp
+++ src/driver_ini_cfd.cpp
@@ -188,7 +188,7 @@ void driver::driver_ini_cfd()
     }
     // ppress->rebalance(p,a,pgc,ppois,psolv,pflow);
     #endif
-    ptstep->start(a,p,pgc,pturb);
+    ptstep->ini(a,p,pgc);
 
     pini->iniphi_io(a,p,pgc);
     pflow->discharge(p,a,pgc);
